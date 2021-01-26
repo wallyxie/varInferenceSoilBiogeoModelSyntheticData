80848b0cfaf49ea9703fd865c8e89d888b3dc0ff
Hua Xie <x@wallyxie.com>
2021-01-26 12:34:13 -0800

    Update var_inference_on_sbm.py

--- python_sde_variational_inference_code/var_inference_on_sbm.py
+++ python_sde_variational_inference_code/var_inference_on_sbm.py
@@ -12,3 +12,8 @@ from sbm_steady_state_init_functions import *
 from sbm_sde import *
 
 device = torch.device("".join(["cuda:",f'{args.CUDA_ID}']) if torch.cuda.is_available() else "cpu")
+
+#Read in data
+obs_df = pd.read_csv('CON_synthetic_sol_df.csv')
+obs_times = torch.Tensor(np.array(obs_df['hour'])) #Extract data observation times.
+obs_means = torch.Tensor(np.array(obs_df.drop(columns = 'hour'))) #Convert C data to tensor.
