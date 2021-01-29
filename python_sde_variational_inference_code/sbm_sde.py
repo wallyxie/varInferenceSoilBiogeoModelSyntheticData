369bdf9a02bd3429c15744bc44acf5a72f93b81d
Hua Xie <x@wallyxie.com>
2021-01-28 19:10:02 -0800

    Combined drift and diffusion functions for litter and system to minimize redundancy because diffusion is drift diagonal

--- python_sde_variational_inference_code/sbm_sde.py
+++ python_sde_variational_inference_code/sbm_sde.py
@@ -8,23 +8,26 @@ from sbm_temp_functions import *
 ##STOCHASTIC DIFFERENTIAL EQUATION MODEL FUNCTIONS##
 ####################################################
 
-def litter_scon(t, batch_size = 3):
+def litter_drift_and_diffusion_scon(t, batch_size = 3):
     '''
-    Returns SCON system exogenous litter input vector. Time t is on hourly scale.
+    Returns SCON system exogenous litter input drift. Time t is on hourly scale.
     '''
     I_S = 0.001 + 0.0005 * torch.sin((2 * math.pi / (24 * 365)) * t) #Exogenous SOC input function
     I_D =  0.0001 + 0.00005 * torch.sin((2 * math.pi / (24 * 365)) * t) #Exogenous DOC input function
     I_0 = torch.zeros_like(I_S, device = I_S.device)
     litter_vector = torch.cat([I_S, I_D, I_0], 1)
-    print('\nlitter_vector size', litter_vector.size())
-    litter_vector_add_dim = litter_vector.repeat(batch_size, 1, 1)
-    return litter_vector_add_dim
+    litter_vector_perm = litter_vector.permute(0, 2, 1)
+    #print('\nlitter_vector size', litter_vector.size())
+    #litter_vector_add_dim = litter_vector.repeat(batch_size, 1, 1)
+    litter_diffusion_matrix_sqrt = torch.zeros([c_vector.size(0), c_vector.size(2), 3, 3], device=c_vector.device) #Create zeros tensor to assign diffusion matrix elements.
+    litter_diffusion_matrix_sqrt[:, :, 0, 0] = torch.sqrt(LowerBound.apply(I_S, 1e-8)).squeeze() #Assigned to element 1, 1 of matrix.
+    litter_diffusion_matrix_sqrt[:, :, 1, 1] = torch.sqrt(LowerBound.apply(I_D, 1e-8)).squeeze() #Assigned to element 2, 2 of matrix.
+    return litter_vector_perm, litter_diffusion_matrix_sqrt  
 
-def drift_scon(c_vector, t, scon_params_dict, temp_ref, batch_size = 3):
+def drift_and_diffusion_scon(c_vector, t, scon_params_dict, temp_ref, batch_size = 3):
     '''
-    Returns SCON system drift vector for approximate p(x).
+    Returns SCON system drift vector and diffusion matrix.
     current_temp is output from temp_gen function. 
-    c_vector[0] is expected to be SOC, c_vector[1] is expected to be DOC, and c_vector[2] is expected to be MBC.
     Expected scon_params_dict = {scon_params_dict = {'u_M': u_M, 'a_SD': a_SD, 'a_DS': a_DS, 'a_M': a_M, 'a_MSC': a_MSC, 'k_S_ref': k_S_ref, 'k_D_ref': k_D_ref, 'k_M_ref': k_M_ref, 'Ea_S': Ea_S, 'Ea_D': Ea_D, 'Ea_M': Ea_M}
     '''
     SOC, DOC, MBC = [c_vector[:, i:i + 1, :] for i in range(batch_size)]
@@ -35,34 +38,16 @@ def drift_scon(c_vector, t, scon_params_dict, temp_ref, batch_size = 3):
     k_D = arrhenius_temp_dep(scon_params_dict['k_D_ref'], current_temp, scon_params_dict['Ea_D'], temp_ref)
     k_M = arrhenius_temp_dep(scon_params_dict['k_M_ref'], current_temp, scon_params_dict['Ea_M'], temp_ref)
     #Drift vector is calculated (without litter input).
-    SOC_drift = scon_params_dict['a_DS'] * k_D * DOC + scon_params_dict['a_M'] * scon_params_dict['a_MSC'] * k_M * MBC - k_S * SOC
-    DOC_drift = scon_params_dict['a_SD'] * k_S * SOC + scon_params_dict['a_M'] * (1 - scon_params_dict['a_MSC']) * k_M * MBC - (scon_params_dict['u_M'] + k_D) * DOC 
-    MBC_drift = scon_params_dict['u_M'] * DOC - k_M * MBC
-    drift_vector = torch.cat([SOC_drift, DOC_drift, MBC_drift], 1)
+    dSOC = scon_params_dict['a_DS'] * k_D * DOC + scon_params_dict['a_M'] * scon_params_dict['a_MSC'] * k_M * MBC - k_S * SOC
+    dDOC = scon_params_dict['a_SD'] * k_S * SOC + scon_params_dict['a_M'] * (1 - scon_params_dict['a_MSC']) * k_M * MBC - (scon_params_dict['u_M'] + k_D) * DOC 
+    dMBC = scon_params_dict['u_M'] * DOC - k_M * MBC
+    drift_vector = torch.cat([dSOC, dDOC, dMBC], 1)
     drift_vector_perm = drift_vector.permute(0, 2, 1)
     #print('\n drift_vector = ', drift_vector)
     #print('\n drift_vector.permute', drift_vector_perm)
-    return drift_vector_perm
-    
-def diffusion_scon(c_vector, t, scon_params_dict, temp_ref, batch_size = 3):
-  '''
-  Returns basic SCON system diffusion matrix in which naive diagonalization is used for stochastic conversion rather than Golightly & Wilkinson reaction network conversion.
-  current_temp is output from temp_gen function.
-  c_vector[0] is expected to be SOC, c_vector[1] is expected to be DOC, and c_vector[2] is expected to be MBC.
-  Expected scon_params_dict = {scon_params_dict = {'u_M': u_M, 'a_SD': a_SD, 'a_DS': a_DS, 'a_M': a_M, 'a_MSC': a_MSC, 'k_S_ref': k_S_ref, 'k_D_ref': k_D_ref, 'k_M_ref': k_M_ref, 'Ea_S': Ea_S, 'Ea_D': Ea_D, 'Ea_M': Ea_M}   
-  '''
-  SOC, DOC, MBC = [c_vector[:, i:i + 1, :] for i in range(batch_size)] #Get current system pool density values from C_vector tensor.
-  current_temp = temp_gen(t, temp_ref) #Obtain temperature from wave function at current time t.
-  #Decay parameters are forced by temperature changes.    
-  k_S = arrhenius_temp_dep(scon_params_dict['k_S_ref'], current_temp, scon_params_dict['Ea_S'], temp_ref)
-  k_D = arrhenius_temp_dep(scon_params_dict['k_D_ref'], current_temp, scon_params_dict['Ea_D'], temp_ref)
-  k_M = arrhenius_temp_dep(scon_params_dict['k_M_ref'], current_temp, scon_params_dict['Ea_M'], temp_ref)
-  #Diffusion matrix is calculated (recall litter input is not a part of the drift vector or diffusion matrix).
-  diffusion_matrix_sqrt = torch.zeros([c_vector.size(0), c_vector.size(2), 3,3], device=c_vector.device) #Create zeros tensor to assign diffusion matrix elements.
-  a11 = scon_params_dict['a_DS'] * k_D * DOC + scon_params_dict['a_M'] * scon_params_dict['a_MSC'] * k_M * MBC - k_S * SOC
-  a22 = scon_params_dict['a_SD'] * k_S * SOC + scon_params_dict['a_M'] * (1 - scon_params_dict['a_MSC']) * k_M * MBC - (scon_params_dict['u_M'] + k_D) * DOC
-  a33 = scon_params_dict['u_M'] * DOC - k_M * MBC
-  diffusion_matrix_sqrt[:, :, 0, 0] = torch.sqrt(LowerBound.apply(a11, 1e-6)).squeeze()
-  diffusion_matrix_sqrt[:, :, 1, 1] = torch.sqrt(LowerBound.apply(a22, 1e-6)).squeeze()
-  diffusion_matrix_sqrt[:, :, 2, 2] = torch.sqrt(LowerBound.apply(a33, 1e-6)).squeeze()
-  return diffusion_matrix_sqrt
+    #Diffusion matrix is calculated (recall litter input is not a part of the drift vector or diffusion matrix).
+    diffusion_matrix_sqrt = torch.zeros([c_vector.size(0), c_vector.size(2), 3, 3], device=c_vector.device) #Create zeros tensor to assign diffusion matrix elements.
+    diffusion_matrix_sqrt[:, :, 0, 0] = torch.sqrt(LowerBound.apply(dSOC, 1e-8)).squeeze() #Assigned to element 1, 1 of matrix.
+    diffusion_matrix_sqrt[:, :, 1, 1] = torch.sqrt(LowerBound.apply(dDOC, 1e-8)).squeeze() #Assigned to element 2, 2 of matrix.
+    diffusion_matrix_sqrt[:, :, 2, 2] = torch.sqrt(LowerBound.apply(dMBC, 1e-8)).squeeze() #Assigned to element 3, 3 of matrix.
+    return drift_vector_perm, diffusion_matrix_sqrt
