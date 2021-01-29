df0874f99b4f4f22286763a204db778c4d599c54
Hua Xie <x@wallyxie.com>
2021-01-28 19:38:03 -0800

    Fixed redundant C_vector assignment

--- python_sde_variational_inference_code/sbm_sde.py
+++ python_sde_variational_inference_code/sbm_sde.py
@@ -8,31 +8,31 @@ from sbm_temp_functions import *
 ##STOCHASTIC DIFFERENTIAL EQUATION MODEL FUNCTIONS##
 ####################################################
 
-def litter_drift_and_diffusion_scon(t, batch_size = 3):
+def litter_drift_and_diffusion_scon(C_vector, T_span, batch_size = 3):
     '''
     Returns SCON system exogenous litter input drift. Time t is on hourly scale.
     '''
-    I_S = 0.001 + 0.0005 * torch.sin((2 * math.pi / (24 * 365)) * t) #Exogenous SOC input function
-    I_D =  0.0001 + 0.00005 * torch.sin((2 * math.pi / (24 * 365)) * t) #Exogenous DOC input function
+    SOC, DOC, MBC = [C_vector[:, i:i + 1, :] for i in range(batch_size)] #Get current system pool density values from C_vector tensor.
+    I_S = 0.001 + 0.0005 * torch.sin((2 * math.pi / (24 * 365)) * T_span) #Exogenous SOC input function
+    I_D =  0.0001 + 0.00005 * torch.sin((2 * math.pi / (24 * 365)) * T_span) #Exogenous DOC input function
     I_0 = torch.zeros_like(I_S, device = I_S.device)
     litter_vector = torch.cat([I_S, I_D, I_0], 1)
     litter_vector_perm = litter_vector.permute(0, 2, 1)
     #print('\nlitter_vector size', litter_vector.size())
     #litter_vector_add_dim = litter_vector.repeat(batch_size, 1, 1)
-    litter_diffusion_matrix_sqrt = torch.zeros([c_vector.size(0), c_vector.size(2), 3, 3], device=c_vector.device) #Create zeros tensor to assign diffusion matrix elements.
+    litter_diffusion_matrix_sqrt = torch.zeros([C_vector.size(0), C_vector.size(2), 3, 3], device = C_vector.device) #Create zeros tensor to assign diffusion matrix elements.
     litter_diffusion_matrix_sqrt[:, :, 0, 0] = torch.sqrt(LowerBound.apply(I_S, 1e-8)).squeeze() #Assigned to element 1, 1 of matrix.
     litter_diffusion_matrix_sqrt[:, :, 1, 1] = torch.sqrt(LowerBound.apply(I_D, 1e-8)).squeeze() #Assigned to element 2, 2 of matrix.
     return litter_vector_perm, litter_diffusion_matrix_sqrt  
 
-def drift_and_diffusion_scon(c_vector, t, scon_params_dict, temp_ref, batch_size = 3):
+def drift_and_diffusion_scon(C_vector, T_span, scon_params_dict, temp_ref, batch_size = 3):
     '''
     Returns SCON system drift vector and diffusion matrix.
     current_temp is output from temp_gen function. 
     Expected scon_params_dict = {scon_params_dict = {'u_M': u_M, 'a_SD': a_SD, 'a_DS': a_DS, 'a_M': a_M, 'a_MSC': a_MSC, 'k_S_ref': k_S_ref, 'k_D_ref': k_D_ref, 'k_M_ref': k_M_ref, 'Ea_S': Ea_S, 'Ea_D': Ea_D, 'Ea_M': Ea_M}
     '''
-    SOC, DOC, MBC = [c_vector[:, i:i + 1, :] for i in range(batch_size)]
-    C_vector = SOC, DOC, MBC #Get current system pool density values from C_vector tensor.
-    current_temp = temp_gen(t, temp_ref) #Obtain temperature from wave function at current time t.
+    SOC, DOC, MBC = [C_vector[:, i:i + 1, :] for i in range(batch_size)]
+    current_temp = temp_gen(T_span, temp_ref) #Obtain temperature from wave function at current time t.
     #Decay parameters are forced by temperature changes.
     k_S = arrhenius_temp_dep(scon_params_dict['k_S_ref'], current_temp, scon_params_dict['Ea_S'], temp_ref)
     k_D = arrhenius_temp_dep(scon_params_dict['k_D_ref'], current_temp, scon_params_dict['Ea_D'], temp_ref)
@@ -46,7 +46,7 @@ def drift_and_diffusion_scon(c_vector, t, scon_params_dict, temp_ref, batch_size
     #print('\n drift_vector = ', drift_vector)
     #print('\n drift_vector.permute', drift_vector_perm)
     #Diffusion matrix is calculated (recall litter input is not a part of the drift vector or diffusion matrix).
-    diffusion_matrix_sqrt = torch.zeros([c_vector.size(0), c_vector.size(2), 3, 3], device=c_vector.device) #Create zeros tensor to assign diffusion matrix elements.
+    diffusion_matrix_sqrt = torch.zeros([C_vector.size(0), C_vector.size(2), 3, 3], device = C_vector.device) #Create zeros tensor to assign diffusion matrix elements.
     diffusion_matrix_sqrt[:, :, 0, 0] = torch.sqrt(LowerBound.apply(dSOC, 1e-8)).squeeze() #Assigned to element 1, 1 of matrix.
     diffusion_matrix_sqrt[:, :, 1, 1] = torch.sqrt(LowerBound.apply(dDOC, 1e-8)).squeeze() #Assigned to element 2, 2 of matrix.
     diffusion_matrix_sqrt[:, :, 2, 2] = torch.sqrt(LowerBound.apply(dMBC, 1e-8)).squeeze() #Assigned to element 3, 3 of matrix.
--- python_sde_variational_inference_code/var_inference_on_sbm.py
+++ python_sde_variational_inference_code/var_inference_on_sbm.py
@@ -11,9 +11,31 @@ from sbm_temp_functions import *
 from sbm_steady_state_init_functions import *
 from sbm_sde import *
 
+torch.manual_seed(0)
+STATE_DIM = 3
+dt = .1
+T = 1000 #Run simulation for 1,000 hours.
+N = int(T / dt)
+T_span = np.linspace(0, T, N)
+T_span_tensor = torch.Tensor(T_span)[(None,) * 2] #T_range needs to be converted to tensor object.
+
+BATCH_SIZE = 1
 device = torch.device("".join(["cuda:",f'{args.CUDA_ID}']) if torch.cuda.is_available() else "cpu")
+LR = 1e-3
+niter = 1000000
 
 #Read in data
 obs_df = pd.read_csv('CON_synthetic_sol_df.csv')
 obs_times = torch.Tensor(np.array(obs_df['hour'])) #Extract data observation times.
 obs_means = torch.Tensor(np.array(obs_df.drop(columns = 'hour'))) #Convert C data to tensor.
+
+def calc_negative_elbo_scon(log_prob, x, x0, T_span, litter_scon, drift_scon, diffusion_scon):
+    #Euler-Maruyama modified from basic form because of exogenous input with separate noise.
+    #euler_maruyama = d.normal.Normal(loc=x[:, :, :-1] + alpha(x[:, :, :-1])*dt, scale = beta_sqrt(x[:, :, :-1])*math.sqrt(dt))
+    #T_range = torch.Tensor(T_range)[(None,) * 2] #T_range needs to be converted to tensor object.
+    x[0] = x0 #Set initial conditions
+    litter_drift, litter_diffusion = litter_drift_and_diffusion_scon(x[:, :, :-1], T_span)
+    system_drift, system_diffusion = drift_and_diffusion_scon(x[:, :, :-1], T_span, scon_params_dict, temp_ref)
+    #euler_maruyama = d.multivariate_normal.MultivariateNormal(loc = x[:, :, :-1].permute(0, 2, 1) + system_drift * dt, scale_tril = system_diffusion * math.sqrt(dt)) + d.multivariate_normal.MultivariateNormal(loc = x[:, :, :-1].permute(0, 2, 1) + litter_drift * dt, scale_tril = litter_diffusion * math.sqrt(dt)) 
+    euler_maruyama = d.multivariate_normal.MultivariateNormal(loc = x[:, :, :-1].permute(0, 2, 1) + system_drift * dt, scale_tril = system_diffusion * math.sqrt(dt)) #Need to add time-dependent exogenous input.
+    return (log_prob - torch.sum(euler_maruyama.log_prob(x[:, :, 1:].permute(0, 2, 1)).sum(-1), -1, keepdim=True)).mean()
