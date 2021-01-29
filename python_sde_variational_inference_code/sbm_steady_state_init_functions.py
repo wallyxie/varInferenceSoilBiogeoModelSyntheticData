f3b12b2353012694e43bb0d30615df9e39beeb2b
Hua Xie <x@wallyxie.com>
2021-01-28 19:57:28 -0800

    Added initial conditions reshape

--- python_sde_variational_inference_code/sbm_steady_state_init_functions.py
+++ python_sde_variational_inference_code/sbm_steady_state_init_functions.py
@@ -6,7 +6,7 @@ import numpy as np
 ##########################################################################
 
 #Analytical_steady_state_init_awb to be coded later.
-def analytical_steady_state_init_con(SOC_input, DOC_input, scon_params_dict):
+def analytical_steady_state_init_con(SOC_input, DOC_input, scon_params_dict, batch_size = 3):
     '''
     Returns a vector of C pool values to initialize an SCON system corresponding to set of parameter values using the analytical steady state equations of the deterministic CON system.
     Vector elements are in order of S_0, D_0, and M_0.
@@ -15,5 +15,7 @@ def analytical_steady_state_init_con(SOC_input, DOC_input, scon_params_dict):
     D_0 = (DOC_input + SOC_input * scon_params_dict['a_SD']) / (scon_params_dict['u_M'] + scon_params_dict['k_D_ref'] + scon_params_dict['u_M'] * scon_params_dict['a_M'] * (scon_params_dict['a_MSC'] - 1 - scon_params_dict['a_MSC'] * scon_params_dict['a_SD']) - scon_params_dict['a_DS'] * scon_params_dict['k_D_ref'] * scon_params_dict['a_SD'])
     S_0 = (SOC_input + D_0 * (scon_params_dict['a_DS'] * scon_params_dict['k_D_ref'] + scon_params_dict['u_M'] * scon_params_dict['a_M'] * scon_params_dict['a_MSC'])) / scon_params_dict['k_S_ref']
     M_0 = scon_params_dict['u_M'] * D_0 / scon_params_dict['k_M_ref']
-    C_0_vector = torch.reshape(torch.FloatTensor([S_0, D_0, M_0]), [3, 1]) #Create tensor object to store column vector of C0 initial conditions. (Can also create zeros tensor and then assign elements.)
+    C_0_vector = torch.as_tensor([S_0, D_0, M_0]).unsqueeze(0)
+    C_0_vector = C_0_vector.resize_((1, 3, 1)) #Need to reshape initial conditions to assign to C_vector.
+    print('C_0_vector size', C_0_vector) #Create tensor object to store vector of C0 initial conditions. Must get to size torch.size(1, 3, 1)
     return C_0_vector
