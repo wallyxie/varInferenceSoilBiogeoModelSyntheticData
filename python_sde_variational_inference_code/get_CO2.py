6d0aa90e878b4bfa32ecee5f09f2641c453d6710
Hua Xie <x@wallyxie.com>
2021-04-15 22:42:39 -0700

    Fixed get_CO2 functions for AWB/AWB_ECA; added AWB_ECA flow test to notebook

--- python_sde_variational_inference_code/get_CO2.py
+++ python_sde_variational_inference_code/get_CO2.py
@@ -18,8 +18,19 @@ def get_CO2_AWB(C_PATH, T_SPAN_TENSOR, SAWB_params_dict, TEMP_GEN, TEMP_REF):
     SOC, DOC, MBC, EEC =  torch.chunk(C_PATH, state_dim, -1) #Partition SOC, DOC, MBC, EEC values. Split based on final C_PATH dim, which specifies state variables and is also indexed as dim #2 in tensor. 
     current_temp = TEMP_GEN(T_SPAN_TENSOR, TEMP_REF)
     #Decay parameters are forced by temperature changes.    
-    u_Q = linear_temp_dep(SAWB_C_params_dict['u_Q_ref'], current_temp, SAWB_C_params_dict['Q'], TEMP_REF) #Apply linear temperature-dependence to u_Q.
-    V_D = arrhenius_temp_dep(SAWB_C_params_dict['V_D_ref'], current_temp, SAWB_C_params_dict['Ea_V_D'], TEMP_REF) #Apply vectorized temperature-dependent transformation to V_D.
-    V_U = arrhenius_temp_dep(SAWB_C_params_dict['V_U_ref'], current_temp, SAWB_C_params_dict['Ea_V_U'], TEMP_REF) #Apply vectorized temperature-dependent transformation to V_U.
-    CO2 = (1 - u_Q) * (V_U * MBC * DOC) / (SAWB_C_params_dict['K_U'] + DOC)
+    u_Q = linear_temp_dep(SAWB_params_dict['u_Q_ref'], current_temp, SAWB_params_dict['Q'], TEMP_REF) #Apply linear temperature-dependence to u_Q.
+    V_D = arrhenius_temp_dep(SAWB_params_dict['V_D_ref'], current_temp, SAWB_params_dict['Ea_V_D'], TEMP_REF) #Apply vectorized temperature-dependent transformation to V_D.
+    V_U = arrhenius_temp_dep(SAWB_params_dict['V_U_ref'], current_temp, SAWB_params_dict['Ea_V_U'], TEMP_REF) #Apply vectorized temperature-dependent transformation to V_U.
+    CO2 = (1 - u_Q) * (V_U * MBC * DOC) / (SAWB_params_dict['K_U'] + MBC + DOC)
+    return LowerBound.apply(CO2, 1e-10)
+
+def get_CO2_AWB_ECA(C_PATH, T_SPAN_TENSOR, SAWB_ECA_params_dict, TEMP_GEN, TEMP_REF):
+    state_dim = 4 #SAWB and SAWB-ECA have four state variables in SOC, DOC, MBC, and EEC.
+    SOC, DOC, MBC, EEC =  torch.chunk(C_PATH, state_dim, -1) #Partition SOC, DOC, MBC, EEC values. Split based on final C_PATH dim, which specifies state variables and is also indexed as dim #2 in tensor. 
+    current_temp = TEMP_GEN(T_SPAN_TENSOR, TEMP_REF)
+    #Decay parameters are forced by temperature changes.    
+    u_Q = linear_temp_dep(SAWB_ECA_params_dict['u_Q_ref'], current_temp, SAWB_ECA_params_dict['Q'], TEMP_REF) #Apply linear temperature-dependence to u_Q.
+    V_DE = arrhenius_temp_dep(SAWB_ECA_params_dict['V_DE_ref'], current_temp, SAWB_ECA_params_dict['Ea_V_DE'], TEMP_REF) #Apply vectorized temperature-dependent transformation to V_DE.
+    V_UE = arrhenius_temp_dep(SAWB_ECA_params_dict['V_UE_ref'], current_temp, SAWB_ECA_params_dict['Ea_V_UE'], TEMP_REF) #Apply vectorized temperature-dependent transformation to V_UE.
+    CO2 = (1 - u_Q) * (V_UE * MBC * DOC) / (SAWB_ECA_params_dict['K_UE'] + MBC + DOC)
     return LowerBound.apply(CO2, 1e-10)
