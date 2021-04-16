15e83129283558b4d8838d3ae4187d0453cd8ca5
Hua Xie <x@wallyxie.com>
2021-04-16 01:31:14 -0700

    Increased LowerBound and flow thresholds because of NaN issues

--- python_sde_variational_inference_code/get_CO2.py
+++ python_sde_variational_inference_code/get_CO2.py
@@ -11,7 +11,7 @@ def get_CO2_CON(C_PATH, T_SPAN_TENSOR, SCON_params_dict, TEMP_GEN, TEMP_REF):
     k_D = arrhenius_temp_dep(SCON_params_dict['k_D_ref'], current_temp, SCON_params_dict['Ea_D'], TEMP_REF) #Apply vectorized temperature-dependent transformation to k_D_ref.
     k_M = arrhenius_temp_dep(SCON_params_dict['k_M_ref'], current_temp, SCON_params_dict['Ea_M'], TEMP_REF) #Apply vectorized temperature-dependent transformation to k_M_ref.
     CO2 = (k_S * SOC * (1 - SCON_params_dict['a_SD'])) + (k_D * DOC * (1 - SCON_params_dict['a_DS'])) + (k_M * MBC * (1 - SCON_params_dict['a_M']))
-    return LowerBound.apply(CO2, 1e-10)
+    return LowerBound.apply(CO2, 1e-7)
 
 def get_CO2_AWB(C_PATH, T_SPAN_TENSOR, SAWB_params_dict, TEMP_GEN, TEMP_REF):
     state_dim = 4 #SAWB and SAWB-ECA have four state variables in SOC, DOC, MBC, and EEC.
@@ -22,7 +22,7 @@ def get_CO2_AWB(C_PATH, T_SPAN_TENSOR, SAWB_params_dict, TEMP_GEN, TEMP_REF):
     V_D = arrhenius_temp_dep(SAWB_params_dict['V_D_ref'], current_temp, SAWB_params_dict['Ea_V_D'], TEMP_REF) #Apply vectorized temperature-dependent transformation to V_D.
     V_U = arrhenius_temp_dep(SAWB_params_dict['V_U_ref'], current_temp, SAWB_params_dict['Ea_V_U'], TEMP_REF) #Apply vectorized temperature-dependent transformation to V_U.
     CO2 = (1 - u_Q) * (V_U * MBC * DOC) / (SAWB_params_dict['K_U'] + MBC + DOC)
-    return LowerBound.apply(CO2, 1e-10)
+    return LowerBound.apply(CO2, 1e-7)
 
 def get_CO2_AWB_ECA(C_PATH, T_SPAN_TENSOR, SAWB_ECA_params_dict, TEMP_GEN, TEMP_REF):
     state_dim = 4 #SAWB and SAWB-ECA have four state variables in SOC, DOC, MBC, and EEC.
@@ -33,4 +33,4 @@ def get_CO2_AWB_ECA(C_PATH, T_SPAN_TENSOR, SAWB_ECA_params_dict, TEMP_GEN, TEMP_
     V_DE = arrhenius_temp_dep(SAWB_ECA_params_dict['V_DE_ref'], current_temp, SAWB_ECA_params_dict['Ea_V_DE'], TEMP_REF) #Apply vectorized temperature-dependent transformation to V_DE.
     V_UE = arrhenius_temp_dep(SAWB_ECA_params_dict['V_UE_ref'], current_temp, SAWB_ECA_params_dict['Ea_V_UE'], TEMP_REF) #Apply vectorized temperature-dependent transformation to V_UE.
     CO2 = (1 - u_Q) * (V_UE * MBC * DOC) / (SAWB_ECA_params_dict['K_UE'] + MBC + DOC)
-    return LowerBound.apply(CO2, 1e-10)
+    return LowerBound.apply(CO2, 1e-7)
