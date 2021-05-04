ec2c1f33bc2185d76a3a4d7367f5b3cecba2265f
Debora Sujono <deborausujono@gmail.com>
2021-05-04 09:14:24 -0700

    Fixed minor bug in plot utils

--- python_sde_variational_inference_code/plot_utils.py
+++ python_sde_variational_inference_code/plot_utils.py
@@ -14,7 +14,7 @@ def plot_post(net, x0, obs_model, state_idx=0, num_samples=20,
     x = torch.cat((x0, x), 1)
     
     q_mean, q_std = x[:, :, state_idx].mean(0).detach(), x[:, :, state_idx].std(0).detach()
-    hours = torch.arange(0, t + 1, dt)
+    hours = torch.arange(0, t + dt, dt)
     plt.plot(hours, q_mean)
     plt.fill_between(hours, q_mean - 2*q_std, q_mean + 2*q_std, alpha=0.5)
     plt.plot(obs_model.times, obs_model.mu[state_idx, :], linestyle='None', marker='o')
