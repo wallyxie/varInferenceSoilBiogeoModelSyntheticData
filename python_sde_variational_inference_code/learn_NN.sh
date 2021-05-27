6125461f55f4ab71fea8291b991714c95ea20442
Hua Xie <x@wallyxie.com>
2021-05-27 14:06:01 -0700

    Add bash scripts to .gitignore

--- python_sde_variational_inference_code/learn_NN.sh
+++ python_sde_variational_inference_code/learn_NN.sh
@@ -1,10 +1,13 @@
 #!/bin/bash
 
-#SBATCH -A allisons_lab_gpu ## charged account
-#SBATCH -p hugemem ## request larger memory per task; highmem, hugemem, and maxmem are options
-#SBATCH --mem 8GB ## total amount of memory requested
+#SBATCH --job-name=learn_NN_test
+#SBATCH --partition=free-gpu      # specify free-gpu partition
+#SBATCH --gres=gpu:V100:1
+#SBATCH -A allisons_lab_gpu # charged account
+#SBATCH -p hugemem # request larger memory per task; highmem, hugemem, and maxmem are options
+#SBATCH --mem 8GB # total amount of memory requested
 #SBATCH -N 1 ## number of nodes
-#SBATCH --ntasks--er-node=1 ## number of tasks per node
-#SBATCH --error=slurm-%J.err ## write errors in slurm-<jobID>.err file
-#SBATCH --mail-type=ALL ## send email for all job milestones
-#SBATCH --mail-user=xiehw@uci.edu ## use this email address
+#SBATCH --ntasks--er-node=1 # number of tasks per node
+#SBATCH --error=slurm-%J.err # write errors in slurm-<jobID>.err file
+#SBATCH --mail-type=ALL # send email for all job milestones
+#SBATCH --mail-user=xiehw@uci.edu # use this email address
