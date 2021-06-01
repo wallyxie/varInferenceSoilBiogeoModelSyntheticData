d763a34a978432867cd3417b2baa8d37c2026191
Hua Xie <x@wallyxie.com>
2021-06-01 10:43:47 -0700

    added niter for Tom

--- pytorch_sbm_sde_vi/learn_NN.py
+++ pytorch_sbm_sde_vi/learn_NN.py
@@ -39,8 +39,8 @@ temp_ref = 283
 temp_rise = 5 #High estimate of 5 celsius temperature rise by 2100.
 
 #Training parameters
-niter = 2
-piter = 1
+niter = 50000
+piter = 500
 pretrain_lr = 1e-2 #Norm regularization learning rate
 train_lr = 1e-3 #ELBO learning rate
 batch_size = 5 #3 - number needed to fit UCI HPC3 RAM requirements with 16 GB RAM.
