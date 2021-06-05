e08d41535a5d9996436f310f57575d274a8f857d
Hua Xie <x@wallyxie.com>
2021-06-04 18:49:00 -0700

    Fixed non-integer indexing issue; changed learning rates

--- pytorch_sbm_sde_vi/learn_NN.py
+++ pytorch_sbm_sde_vi/learn_NN.py
@@ -28,7 +28,7 @@ if torch.cuda.is_available():
     torch.set_default_tensor_type('torch.cuda.FloatTensor')
 
 #Neural SDE parameters
-dt_flow = 0.1 #Increased from 0.1 to reduce memory.
+dt_flow = 0.2 #Increased from 0.1 to reduce memory.
 t = 1500 #5000. Reduced to see impact on memory. #In hours.
 n = int(t / dt_flow) + 1
 t_span = np.linspace(0, t, n)
@@ -41,11 +41,11 @@ temp_ref = 283
 temp_rise = 5 #High estimate of 5 celsius temperature rise by 2100.
 
 #Training parameters
-niter = 9500
-piter = 600
+niter = 11000
+piter = 1000
 pretrain_lr = 1e-4 #Norm regularization learning rate
-train_lr = 1e-4 #ELBO learning rate
-batch_size = 5 #3 - number needed to fit UCI HPC3 RAM requirements with 16 GB RAM at t = 5000.
+train_lr = 8e-5 #ELBO learning rate
+batch_size = 4 #3 - number needed to fit UCI HPC3 RAM requirements with 16 GB RAM at t = 5000.
 eval_batch_size = 5
 obs_error_scale = 0.1 #Observation (y) standard deviation.
 prior_scale_factor = 0.1 #Proportion of prior standard deviation to prior means.
@@ -114,5 +114,5 @@ ELBO_hist = torch.load(ELBO_save_string)
 #Plot training posterior results and ELBO history.
 net.eval()
 x, _ = net(eval_batch_size)
-plot_elbo(ELBO_hist, niter, t, dt_flow, batch_size, eval_batch_size, num_layers, now_string, xmin = (niter - piter) * 0.25) #xmin < niter.
+plot_elbo(ELBO_hist, niter, t, dt_flow, batch_size, eval_batch_size, num_layers, now_string, xmin = 500) #xmin < niter.
 plot_states_post(x, obs_model_noCO2, niter, t, dt_flow, batch_size, eval_batch_size, num_layers, now_string, ymin_list = [0, 0, 0], ymax_list = [80, 2.8, 5.0])
