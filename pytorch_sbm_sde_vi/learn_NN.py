5865dd7781e3db7674563baf2103fc7bd504c158
Hua Xie <x@wallyxie.com>
2021-06-10 21:12:30 -0700

    decrease LR again to try to get non-zero path results

--- pytorch_sbm_sde_vi/learn_NN.py
+++ pytorch_sbm_sde_vi/learn_NN.py
@@ -29,8 +29,8 @@ if torch.cuda.is_available():
     torch.set_default_tensor_type('torch.cuda.FloatTensor')
 
 #Neural SDE parameters
-dt_flow = 0.25 #Increased from 0.1 to reduce memory.
-t = 2500 #5000. Reduced to see impact on memory. #In hours.
+dt_flow = 0.2 #Increased from 0.1 to reduce memory.
+t = 1900 #5000. Reduced to see impact on memory. #In hours.
 n = int(t / dt_flow) + 1
 t_span = np.linspace(0, t, n)
 t_span_tensor = torch.reshape(torch.Tensor(t_span), [1, n, 1]).to(active_device) #T_span needs to be converted to tensor object. Additionally, facilitates conversion of I_S and I_D to tensor objects.
@@ -42,11 +42,11 @@ temp_ref = 283
 temp_rise = 5 #High estimate of 5 celsius temperature rise by 2100.
 
 #Training parameters
-niter = 9001 #Total number of training iterations, including ELBO iterations.
-piter = 250 #Number of pre-training iterations.
-pretrain_lr = 1e-2 #Norm regularization learning rate
-train_lr = 1e-2 #ELBO learning rate
-batch_size = 5 #3 - number needed to fit UCI HPC3 RAM requirements with 16 GB RAM at t = 5000.
+niter = 5001 #Total number of training iterations, including ELBO iterations.
+piter = 550 #Number of pre-training iterations.
+pretrain_lr = 2e-3 #Norm regularization learning rate
+train_lr = 1e-3 #ELBO learning rate
+batch_size = 6 #3 - number needed to fit UCI HPC3 RAM requirements with 16 GB RAM at t = 5000.
 eval_batch_size = 5
 obs_error_scale = 0.1 #Observation (y) standard deviation.
 prior_scale_factor = 0.1 #Proportion of prior standard deviation to prior means.
