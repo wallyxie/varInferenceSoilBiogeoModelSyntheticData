22295df7adb108fdb722690f240b0a5580ce50ea
Hua Xie <x@wallyxie.com>
2021-06-14 14:16:02 -0700

    Update learn_NN.py

--- pytorch_sbm_sde_vi/learn_NN.py
+++ pytorch_sbm_sde_vi/learn_NN.py
@@ -30,7 +30,7 @@ if torch.cuda.is_available():
 
 #Neural SDE parameters
 dt_flow = 0.5 #Increased from 0.1 to reduce memory.
-t = 2200 #5000. Reduced to see impact on memory. #In hours.
+t = 2500 #5000. Reduced to see impact on memory. #In hours.
 n = int(t / dt_flow) + 1
 t_span = np.linspace(0, t, n)
 t_span_tensor = torch.reshape(torch.Tensor(t_span), [1, n, 1]).to(active_device) #T_span needs to be converted to tensor object. Additionally, facilitates conversion of I_S and I_D to tensor objects.
@@ -42,10 +42,10 @@ temp_ref = 283
 temp_rise = 5 #High estimate of 5 celsius temperature rise by 2100.
 
 #Training parameters
-niter = 5000 #Total number of training iterations, including ELBO iterations.
-piter = 0 #Number of pre-training iterations.
-pretrain_lr = 8e-3 #Norm regularization learning rate
-train_lr = 2e-3 #ELBO learning rate
+niter = 6600 #Total number of training iterations, including ELBO iterations.
+piter = 100 #Number of pre-training iterations.
+pretrain_lr = 5e-3 #Norm regularization learning rate
+train_lr = 1e-3 #ELBO learning rate
 batch_size = 5 #3 - number needed to fit UCI HPC3 RAM requirements with 16 GB RAM at t = 5000.
 eval_batch_size = 5
 obs_error_scale = 0.1 #Observation (y) standard deviation.
