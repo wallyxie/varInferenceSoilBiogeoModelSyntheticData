3056ff68c1ea56a4adc2bf0c8ca57d7118b467ba
Hua Xie <x@wallyxie.com>
2021-07-06 18:21:31 -0700

    modified save_string

--- pytorch_sbm_sde_vi/learn_theta_trunc.py
+++ pytorch_sbm_sde_vi/learn_theta_trunc.py
@@ -44,12 +44,12 @@ temp_ref = 283
 temp_rise = 5 #High estimate of 5 celsius temperature rise by 2100.
 
 #Training parameters
-niter = 10000
+niter = 100000
 piter = 0
 pretrain_lr = 1e-3 #Norm regularization learning rate
-train_lr = 5e-5 #ELBO learning rate
-batch_size = 10 #3 - number needed to fit UCI HPC3 RAM requirements with 16 GB RAM at t = 5000.
-eval_batch_size = 10
+train_lr = 2e-5 #ELBO learning rate
+batch_size = 11 #3 - number needed to fit UCI HPC3 RAM requirements with 16 GB RAM at t = 5000.
+eval_batch_size = 11
 obs_error_scale = 0.1 #Observation (y) standard deviation.
 prior_scale_factor = 0.1 #Proportion of prior standard deviation to prior means.
 num_layers = 5 #5 - number needed to fit UCI HPC3 RAM requirements with 16 GB RAM at t = 5000.
@@ -102,7 +102,8 @@ net, obs_model, ELBO_hist, list_theta, list_parent_loc_scale = train(active_devi
 
 #Save net and ELBO files.
 now = datetime.now()
-now_string = now.strftime("%Y_%m_%d_%H_%M_%S")
+now_string = 'trunc_' + now.strftime('%Y_%m_%d_%H_%M_%S')
+
 save_string = f'out_iter_{niter}_t_{t}_dt_{dt_flow}_batch_{batch_size}_layers_{num_layers}_{now_string}.pt'
 torch.save((net, ELBO_hist, list_theta, list_parent_loc_scale), save_string)
 
