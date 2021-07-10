060c6717102ae3527f1d1954fe4e368149081854
Hua Xie <x@wallyxie.com>
2021-07-09 18:13:01 -0700

    Update learn_theta_trunc.py

--- pytorch_sbm_sde_vi/learn_theta_trunc.py
+++ pytorch_sbm_sde_vi/learn_theta_trunc.py
@@ -32,7 +32,7 @@ torch.set_printoptions(precision = 8)
 
 #Neural SDE parameters
 dt_flow = 0.5 #Increased from 0.1 to reduce memory.
-t = 400 #2000. #In hours.
+t = 350 #2000. #In hours.
 n = int(t / dt_flow) + 1
 t_span = np.linspace(0, t, n)
 t_span_tensor = torch.reshape(torch.Tensor(t_span), [1, n, 1]).to(active_device) #T_span needs to be converted to tensor object. Additionally, facilitates conversion of I_S and I_D to tensor objects.
@@ -48,8 +48,8 @@ niter = 1500000
 piter = 0
 pretrain_lr = 1e-3 #Norm regularization learning rate
 train_lr = 1e-5 #ELBO learning rate
-batch_size = 10 #3 - number needed to fit UCI HPC3 RAM requirements with 16 GB RAM at t = 5000.
-eval_batch_size = 10
+batch_size = 12 #3 - number needed to fit UCI HPC3 RAM requirements with 16 GB RAM at t = 5000.
+eval_batch_size = 12
 obs_error_scale = 0.1 #Observation (y) standard deviation.
 prior_scale_factor = 0.1 #Proportion of prior standard deviation to prior means.
 num_layers = 5 #5 - number needed to fit UCI HPC3 RAM requirements with 16 GB RAM at t = 5000.
