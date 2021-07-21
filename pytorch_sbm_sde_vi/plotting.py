aea22aa9182710ae50e5d5341f3a591d13a1743b
Hua Xie <x@wallyxie.com>
2021-07-21 07:11:45 -0700

    Pushed fix for swapping of posterior and prior in plotting.

--- pytorch_sbm_sde_vi/plotting.py
+++ pytorch_sbm_sde_vi/plotting.py
@@ -60,8 +60,8 @@ def plot_theta(p_theta, q_theta, niter, piter, t, dt, batch_size, eval_batch_siz
     x = torch.zeros([num_pts, loc.size(0)]) #Examining densities as we move through distribution supports. So torch.Size([bins, parameters]) is desired size of x.
     for param_index in range(0, loc.size(0)):
         x[:, param_index] = torch.linspace(lower[param_index], upper[param_index], num_pts)
-    pdf_prior = torch.exp(q_dist.log_prob(x)).detach()
-    pdf_post = torch.exp(p_dist.log_prob(x)).detach()
+    pdf_prior = torch.exp(p_dist.log_prob(x)).detach()
+    pdf_post = torch.exp(q_dist.log_prob(x)).detach()
 
     #Find appropriate plotting range of x based on pdf density concentration (where pdf > 1 for prior and post).    
     prior_first_one_indices = torch.zeros(loc.size(0))
