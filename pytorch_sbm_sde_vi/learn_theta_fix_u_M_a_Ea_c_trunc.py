4082c3b12ba177dcb53b60221e7132da2147eec0
Hua Xie <x@wallyxie.com>
2021-08-19 16:20:59 -0700

    Needed to apply LowerBound to parent locs for fix_u_M_a_Ea_c and drop LR.

--- pytorch_sbm_sde_vi/learn_theta_fix_u_M_a_Ea_c_trunc.py
+++ pytorch_sbm_sde_vi/learn_theta_fix_u_M_a_Ea_c_trunc.py
@@ -50,10 +50,10 @@ temp_ref = 283
 temp_rise = 5 #High estimate of 5 celsius temperature rise by 2100.
 
 #Training parameters
-niter = 355000
+niter = 440000
 piter = 0
 pretrain_lr = 1e-3 #Norm regularization learning rate
-train_lr = 5e-4 #ELBO learning rate
+train_lr = 1e-4 #ELBO learning rate
 batch_size = 32 #3 - number needed to fit UCI HPC3 RAM requirements with 16 GB RAM at t = 5000.
 eval_batch_size = 32
 obs_error_scale = 0.1 #Observation (y) standard deviation.
@@ -95,7 +95,7 @@ net, q_theta, p_theta, obs_model, ELBO_hist, list_parent_loc_scale = train(
         state_dim_SCON, csv_data_path, obs_error_scale, t, dt_flow, n, 
         t_span_tensor, i_s_tensor, i_d_tensor, temp_tensor, temp_ref,
         drift_diffusion_SCONR_C_fix_u_M_a_Ea_c, x0_prior_SCON, SCONR_C_fix_u_M_a_Ea_c_priors_details, theta_dist,
-        LEARN_THETA = True, LR_DECAY = 0.85, DECAY_STEP_SIZE = 50000, PRINT_EVERY = 100)
+        LEARN_THETA = True, LR_DECAY = 0.9, DECAY_STEP_SIZE = 50000, PRINT_EVERY = 100)
 
 #Save net and ELBO files.
 now = datetime.now()
