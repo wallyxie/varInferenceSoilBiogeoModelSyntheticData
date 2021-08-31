Fixed mean_field.py multivariate case log_q_theta calculation

pytorch_sbm_sde_vi/mean_field.py
@@ -49,9 +49,9 @@ class MeanField(nn.Module):
         self.learn_cov = LEARN_COV
         self.means = nn.Parameter(torch.Tensor(means).to(DEVICE))
         if LEARN_COV:
-            self.sds = nn.Parameter(torch.diag(torch.Tensor(sds)).to(DEVICE))
+            self.sds = nn.Parameter(torch.diag(torch.Tensor(sds)).to(DEVICE)) # (num_params, num_params)
         else:
-            self.sds = nn.Parameter(torch.Tensor(sds).to(DEVICE))
+            self.sds = nn.Parameter(torch.Tensor(sds).to(DEVICE)) # (num_params, )
         self.lowers = torch.Tensor(lower_bounds).to(DEVICE)
         self.uppers = torch.Tensor(upper_bounds).to(DEVICE)
         
@@ -70,10 +70,13 @@ class MeanField(nn.Module):
             q_dist = self.dist(parent_loc, parent_scale, a = self.lowers, b = self.uppers)
 
         # Sample theta ~ q(theta).
-        samples = q_dist.rsample([N])
+        samples = q_dist.rsample([N]) # (N, num_params)
         
         # Evaluate log prob of theta samples.
-        log_q_theta = torch.sum(q_dist.log_prob(samples), -1)
+        if self.learn_cov:
+            log_q_theta = q_dist.log_prob(samples) # (N, )
+        else:
+            log_q_theta = torch.sum(q_dist.log_prob(samples), -1) # (N, )
         
         # Return samples in same dictionary format.
         dict_out = {} #Define dictionary with n samples for each parameter.

pytorch_sbm_sde_vi/obs_and_flow.py
@@ -237,7 +237,7 @@ class SDEFlow(nn.Module):
             self.i_tensor = torch.stack((I_S_TENSOR.reshape(-1), I_D_TENSOR.reshape(-1)))[None, :, :].repeat_interleave(3, -1)
 
         self.base_dist = D.normal.Normal(loc = 0., scale = 1.)
-        self.cond_inputs = cond_inputs        
+        self.cond_inputs = cond_inputs
         self.num_layers = num_layers
 
         self.coupling = nn.ModuleList([CouplingLayer(cond_inputs + self.obs_model.obs_dim, 1) for _ in range(num_layers)])