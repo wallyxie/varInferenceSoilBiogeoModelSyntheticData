4011643ff6f5cdf867176d29270235bed4b9f3f1
Hua Xie <x@wallyxie.com>
2021-09-16 21:55:43 -0700

    Pruned outdated learn_theta scripts along with the superseded SBM_SDE_tensor.

--- pytorch_sbm_sde_vi/learn_theta_trunc_SCON-C_CO2_train1.py
+++ pytorch_sbm_sde_vi/learn_theta_trunc_SCON-C_CO2_train1.py
@@ -50,10 +50,10 @@ temp_ref = 283
 temp_rise = 5 #High estimate of 5 celsius temperature rise by 2100.
 
 #Training parameters
-niter = 220
+niter = 200
 train_lr = 2e-5 #ELBO learning rate
-batch_size = 20 #3 - number needed to fit UCI HPC3 RAM requirements with 16 GB RAM at t = 5000.
-eval_batch_size = 20
+batch_size = 50 #3 - number needed to fit UCI HPC3 RAM requirements with 16 GB RAM at t = 5000.
+eval_batch_size = 50
 obs_error_scale = 0.1 #Observation (y) standard deviation.
 prior_scale_factor = 0.333 #Proportion of prior standard deviation to prior means.
 num_layers = 5 #5 - number needed to fit UCI HPC3 RAM requirements with 16 GB RAM at t = 5000.
