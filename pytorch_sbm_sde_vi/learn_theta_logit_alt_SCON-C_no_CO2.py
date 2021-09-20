917000df68ee7a64beca83426c5d938bb20779a2
Hua Xie <x@wallyxie.com>
2021-09-19 18:26:16 -0700

    Match learn_theta_trunc_SCON-C-no_CO2 hyperparameters to compare.

--- pytorch_sbm_sde_vi/learn_theta_logit_alt_SCON-C_no_CO2.py
+++ pytorch_sbm_sde_vi/learn_theta_logit_alt_SCON-C_no_CO2.py
@@ -50,9 +50,9 @@ temp_rise = 5 #High estimate of 5 celsius temperature rise by 2100.
 
 #Training parameters
 niter = 90000
-train_lr = 2e-5 #ELBO learning rate
-batch_size = 50 #3 - number needed to fit UCI HPC3 RAM requirements with 16 GB RAM at t = 5000.
-eval_batch_size = 50
+train_lr = 5e-4 #ELBO learning rate
+batch_size = 30 #3 - number needed to fit UCI HPC3 RAM requirements with 16 GB RAM at t = 5000.
+eval_batch_size = 30
 obs_error_scale = 0.1 #Observation (y) standard deviation.
 prior_scale_factor = 0.333 #Proportion of prior standard deviation to prior means.
 num_layers = 5 #5 - number needed to fit UCI HPC3 RAM requirements with 16 GB RAM at t = 5000.
