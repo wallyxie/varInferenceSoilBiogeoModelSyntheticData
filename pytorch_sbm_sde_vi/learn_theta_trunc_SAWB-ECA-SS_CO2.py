033e05e3d319d79bbeacaebba0c370cfde523793
Hua Xie <x@wallyxie.com>
2021-09-22 20:42:44 -0700

    Last working checkpoint on HPC.

--- pytorch_sbm_sde_vi/learn_theta_trunc_SAWB-ECA-SS_CO2.py
+++ pytorch_sbm_sde_vi/learn_theta_trunc_SAWB-ECA-SS_CO2.py
@@ -49,10 +49,10 @@ temp_ref = 283
 temp_rise = 5 #High estimate of 5 celsius temperature rise by 2100.
 
 #Training parameters
-niter = 290000
+niter = 300000
 train_lr = 2e-5 #ELBO learning rate
-batch_size = 40 #3 - number needed to fit UCI HPC3 RAM requirements with 16 GB RAM at t = 5000.
-eval_batch_size = 40
+batch_size = 35 #3 - number needed to fit UCI HPC3 RAM requirements with 16 GB RAM at t = 5000.
+eval_batch_size = 35
 obs_error_scale = 0.1 #Observation (y) standard deviation.
 prior_scale_factor = 0.333 #Proportion of prior standard deviation to prior means.
 num_layers = 5 #5 - number needed to fit UCI HPC3 RAM requirements with 16 GB RAM at t = 5000.
