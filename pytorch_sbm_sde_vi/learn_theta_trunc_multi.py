439e76b8da1cbf1a89d13fad978d462315fd4d48
Hua Xie <x@wallyxie.com>
2021-09-27 18:21:18 -0700

    Added initial SCON-SS y data with equivalent non-diffusion parameter distributions corresponding to current SCON-C y.

--- pytorch_sbm_sde_vi/learn_theta_trunc_multi.py
+++ pytorch_sbm_sde_vi/learn_theta_trunc_multi.py
@@ -48,10 +48,10 @@ temp_ref = 283
 temp_rise = 5 #High estimate of 5 celsius temperature rise by 2100.
 
 #Training parameters
-niter = 280000 #550000
+niter = 250000 #550000
 train_lr = 5e-4 #ELBO learning rate
-batch_size = 40 #3 - number needed to fit UCI HPC3 RAM requirements with 16 GB RAM at t = 5000.
-eval_batch_size = 40
+batch_size = 45 #3 - number needed to fit UCI HPC3 RAM requirements with 16 GB RAM at t = 5000.
+eval_batch_size = 45
 obs_error_scale = 0.1 #Observation (y) standard deviation.
 prior_scale_factor = 0.333 #Proportion of prior standard deviation to prior means.
 num_layers = 5 #5 - number needed to fit UCI HPC3 RAM requirements with 16 GB RAM at t = 5000.
