5822a785571706b51a620e835d07e1b86346cec7
Hua Xie <x@wallyxie.com>
2021-10-07 14:35:56 -0700

    Deleted no longer applicable comment.

--- pytorch_sbm_sde_vi/learn_theta_trunc_SCON-SS_CO2.py
+++ pytorch_sbm_sde_vi/learn_theta_trunc_SCON-SS_CO2.py
@@ -51,7 +51,7 @@ temp_rise = 5 #High estimate of 5 celsius temperature rise by 2100.
 #Training parameters
 niter = 300000
 train_lr = 2e-5 #ELBO learning rate
-batch_size = 50 #3 - number needed to fit UCI HPC3 RAM requirements with 16 GB RAM at t = 5000.
+batch_size = 50
 eval_batch_size = 50
 obs_error_scale = 0.1 #Observation (y) standard deviation.
 prior_scale_factor = 0.333 #Proportion of prior standard deviation to prior means.
