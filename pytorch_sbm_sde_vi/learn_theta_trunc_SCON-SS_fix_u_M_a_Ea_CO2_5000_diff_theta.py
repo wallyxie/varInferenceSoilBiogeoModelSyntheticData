63e2e0e010dfa722f7bbac04889f4f5f523ee70e
Hua Xie <x@wallyxie.com>
2021-10-20 00:27:40 -0700

    Fixed files used in SCON-SS fix_u_M_a_Ea diff theta experiment script.

--- pytorch_sbm_sde_vi/learn_theta_trunc_SCON-SS_fix_u_M_a_Ea_CO2_5000_diff_theta.py
+++ pytorch_sbm_sde_vi/learn_theta_trunc_SCON-SS_fix_u_M_a_Ea_CO2_5000_diff_theta.py
@@ -64,9 +64,9 @@ diffusion_type = 'SS'
 learn_CO2 = True
 theta_dist = 'TruncatedNormal' #String needs to be exact name of the distribution class. Options are 'TruncatedNormal' and 'RescaledLogitNormal'.
 
-SCON_SS_priors_details = {k: v.to(active_device) for k, v in torch.load('generated_data/SCON-SS_fix_u_M_a_Ea_CO2_trunc_5000_diff_theta_2021_10_19_17_24_sample_y_t_5000_dt_0-01_sd_scale_0-333_hyperparams.pt').items()}
+SCON_SS_priors_details = {k: v.to(active_device) for k, v in torch.load('generated_data/SCON-SS_fix_u_M_a_Ea_CO2_trunc_5000_diff_theta_2021_10_20_00_16_sample_y_t_5000_dt_0-01_sd_scale_0-333_hyperparams.pt').items()}
 
-SCON_SS_fix_u_M_a_Ea_dict = {k: v.to(active_device) for k, v in torch.load('generated_data/SCON-SS_fix_u_M_a_Ea_CO2_trunc_5000_diff_theta_2021_10_19_17_24_sample_y_t_5000_dt_0-01_sd_scale_0-333_fix_dict.pt').items()}
+SCON_SS_fix_u_M_a_Ea_dict = {k: v.to(active_device) for k, v in torch.load('generated_data/SCON-SS_fix_u_M_a_Ea_CO2_trunc_5000_diff_theta_2021_10_20_00_16_sample_y_t_5000_dt_0-01_sd_scale_0-333_fix_dict.pt').items()}
 
 #Initial condition prior means
 x0_SCON = [60, 18, 8]
@@ -82,7 +82,7 @@ i_s_tensor = i_s(t_span_tensor).to(active_device) #Exogenous SOC input function
 i_d_tensor = i_d(t_span_tensor).to(active_device) #Exogenous DOC input function
 
 #Generate observation model.
-csv_data_path = os.path.join('generated_data/', 'SCON-SS_fix_u_M_a_Ea_CO2_trunc_5000_diff_theta_2021_10_19_17_24_sample_y_t_5000_dt_0-01_sd_scale_0-333.csv')
+csv_data_path = os.path.join('generated_data/', 'SCON-SS_fix_u_M_a_Ea_CO2_trunc_5000_diff_theta_2021_10_20_00_16_sample_y_t_5000_dt_0-01_sd_scale_0-333.csv')
 
 #Call training loop function for SCON-SS.
 net, q_theta, p_theta, obs_model, ELBO_hist, list_parent_loc_scale, SBM_SDE_instance = train2(
@@ -125,7 +125,7 @@ obs_model = torch.load(obs_model_save_string)
 obs_model.to(active_device)
 ELBO_hist = torch.load(ELBO_save_string)
 SBM_SDE_instance = torch.load(SBM_SDE_instance_save_string)
-true_theta = torch.load('generated_data/SCON-SS_fix_u_M_a_Ea_CO2_trunc_5000_diff_theta_2021_10_19_17_24_sample_y_t_5000_dt_0-01_sd_scale_0-333_rsample.pt', map_location = active_device)
+true_theta = torch.load('generated_data/SCON-SS_fix_u_M_a_Ea_CO2_trunc_5000_diff_theta_2021_10_20_00_16_sample_y_t_5000_dt_0-01_sd_scale_0-333_rsample.pt', map_location = active_device)
 
 #Plot training posterior results and ELBO history.
 net.eval()
