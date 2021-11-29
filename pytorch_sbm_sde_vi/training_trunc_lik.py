93df11cc4bc0bc69e1c6197dc0375229fa054d10
Debora Sujono <deborausujono@gmail.com>
2021-11-29 10:32:50 -0800

    Minibatch training script in training_trunc_lik.py

--- pytorch_sbm_sde_vi/training_trunc_lik.py
+++ pytorch_sbm_sde_vi/training_trunc_lik.py
@@ -28,71 +28,77 @@ TupleOfTensors = Tuple[torch.Tensor, torch.Tensor]
 ##TRAINING AND ELBO FUNCTIONS##
 ###############################
 
-def calc_log_lik1(C_PATH: torch.Tensor,
+def calc_log_lik(C_PATH: torch.Tensor, # (batch_size, minibatch_size + 1, state_dim)
         PARAMS_DICT: DictOfTensors,
         DT: float, 
         SBM_SDE_CLASS, 
         INIT_PRIOR,
+        LIDX, RIDX
         ):
-    if TRANS_DIST == 'Normal':
-        drift, diffusion_sqrt = SBM_SDE_CLASS.drift_diffusion(C_PATH, PARAMS_DICT) #Appropriate indexing of tensors corresponding to data generating process now handled in `drift_diffusion` class method. Recall that drift diffusion will use C_PATH[:, :-1, :], I_S_TENSOR[:, 1:, :], I_D_TENSOR[:, 1:, :], TEMP_TENSOR[:, 1:, :]. 
+    if LIK_DIST == 'Normal':
+        drift, diffusion_sqrt = SBM_SDE_CLASS.drift_diffusion(C_PATH, PARAMS_DICT, LIDX, RIDX) #Appropriate indexing of tensors corresponding to data generating process now handled in `drift_diffusion` class method. Recall that drift diffusion will use C_PATH[:, :-1, :], I_S_TENSOR[:, 1:, :], I_D_TENSOR[:, 1:, :], TEMP_TENSOR[:, 1:, :]. 
         euler_maruyama_state_sample_object = D.multivariate_normal.MultivariateNormal(loc = C_PATH[:, :-1, :] + drift * DT, scale_tril = diffusion_sqrt * math.sqrt(DT)) #C_PATH[:, :-1, :] + drift * DT will diverge from C_PATH if C_PATH values not compatible with x0 and theta. Algorithm aims to minimize gap between computed drift and actual gradient between x_n and x_{n+1}. 
         
         # Compute log p(x|theta) = log p(x|x0, theta) + log p(x0|theta)
         ll = euler_maruyama_state_sample_object.log_prob(C_PATH[:, 1:, :]).sum(-1) # log p(x|x0, theta)
-        ll += INIT_PRIOR.log_prob(C_PATH[:, 0, :]) # log p(x0|theta)
-    elif TRANS_DIST == 'TruncatedNormal':
-        drift, diffusion_sqrt = SBM_SDE_CLASS.drift_diffusion(C_PATH, PARAMS_DICT, diffusion_matrix=False)
+        if LIDX == 0:
+            ll += INIT_PRIOR.log_prob(C_PATH[:, 0, :]) # log p(x0|theta)
+    elif LIK_DIST == 'TruncatedNormal':
+        drift, diffusion_sqrt = SBM_SDE_CLASS.drift_diffusion(C_PATH, PARAMS_DICT, LIDX, RIDX, diffusion_matrix=False)
         euler_maruyama_state_sample_object = TruncatedNormal(loc = C_PATH[:, :-1, :] + drift * DT, scale = diffusion_sqrt * math.sqrt(DT), a = 0, b = float('inf'))
 
         # Compute log p(x|theta) = log p(x|x0, theta) + log p(x0|theta)
         ll = euler_maruyama_state_sample_object.log_prob(C_PATH[:, 1:, :]).sum((-2, -1)) # log p(x|x0, theta)
-        ll += INIT_PRIOR.log_prob(C_PATH[:, 0, :]).sum(-1) # log p(x0|theta)
+        if LIDX == 0:
+            ll += INIT_PRIOR.log_prob(C_PATH[:, 0, :]).sum(-1) # log p(x0|theta)
 
     return ll, drift, diffusion_sqrt # ll.shape == (state_dim, )
 
-def calc_log_lik2(C_PATH: torch.Tensor,
+def calc_log_lik_CO2(C_PATH: torch.Tensor,
         PARAMS_DICT: DictOfTensors,
         DT: float, 
         SBM_SDE_CLASS, 
         INIT_PRIOR,
-        LEARN_CO2
+        #LEARN_CO2,
+        LIDX, RIDX
         ):
-    if LEARN_CO2:
-        if TRANS_DIST == 'Normal':
-            drift, diffusion_sqrt, x_add_CO2 = SBM_SDE_CLASS.drift_diffusion_add_CO2(C_PATH, PARAMS_DICT) #Appropriate indexing of tensors corresponding to data generating process now handled in `drift_diffusion` class method. Recall that drift diffusion will use C_PATH[:, :-1, :], I_S_TENSOR[:, 1:, :], I_D_TENSOR[:, 1:, :], TEMP_TENSOR[:, 1:, :]. 
-            euler_maruyama_state_sample_object = D.multivariate_normal.MultivariateNormal(loc = C_PATH[:, :-1, :] + drift * DT, scale_tril = diffusion_sqrt * math.sqrt(DT)) #C_PATH[:, :-1, :] + drift * DT will diverge from C_PATH if C_PATH values not compatible with x0 and theta. Algorithm aims to minimize gap between computed drift and actual gradient between x_n and x_{n+1}. 
-        
-            # Compute log p(x|theta) = log p(x|x0, theta) + log p(x0|theta)
-            ll = euler_maruyama_state_sample_object.log_prob(C_PATH[:, 1:, :]).sum(-1) # log p(x|x0, theta)
-            ll += INIT_PRIOR.log_prob(C_PATH[:, 0, :]) # log p(x0|theta)
-        elif TRANS_DIST == 'TruncatedNormal':
-            drift, diffusion_sqrt, x_add_CO2 = SBM_SDE_CLASS.drift_diffusion_add_CO2(C_PATH, PARAMS_DICT, diffusion_matrix=False)
-            euler_maruyama_state_sample_object = TruncatedNormal(loc = C_PATH[:, :-1, :] + drift * DT, scale = diffusion_sqrt * math.sqrt(DT), a = 0, b = float('inf'))
-
-            # Compute log p(x|theta) = log p(x|x0, theta) + log p(x0|theta)
-            ll = euler_maruyama_state_sample_object.log_prob(C_PATH[:, 1:, :]).sum((-2, -1)) # log p(x|x0, theta)
-            ll += INIT_PRIOR.log_prob(C_PATH[:, 0, :]).sum(-1) # log p(x0|theta)        
-
-        return ll, drift, diffusion_sqrt, x_add_CO2
-    else:
-        drift, diffusion_sqrt = SBM_SDE_CLASS.drift_diffusion(C_PATH, PARAMS_DICT) #Appropriate indexing of tensors corresponding to data generating process now handled in `drift_diffusion` class method. Recall that drift diffusion will use C_PATH[:, :-1, :], I_S_TENSOR[:, 1:, :], I_D_TENSOR[:, 1:, :], TEMP_TENSOR[:, 1:, :]. 
+    #if LEARN_CO2:
+    if LIK_DIST == 'Normal':
+        drift, diffusion_sqrt, x_add_CO2 = SBM_SDE_CLASS.drift_diffusion_add_CO2(C_PATH, PARAMS_DICT, LIDX, RIDX) #Appropriate indexing of tensors corresponding to data generating process now handled in `drift_diffusion` class method. Recall that drift diffusion will use C_PATH[:, :-1, :], I_S_TENSOR[:, 1:, :], I_D_TENSOR[:, 1:, :], TEMP_TENSOR[:, 1:, :]. 
         euler_maruyama_state_sample_object = D.multivariate_normal.MultivariateNormal(loc = C_PATH[:, :-1, :] + drift * DT, scale_tril = diffusion_sqrt * math.sqrt(DT)) #C_PATH[:, :-1, :] + drift * DT will diverge from C_PATH if C_PATH values not compatible with x0 and theta. Algorithm aims to minimize gap between computed drift and actual gradient between x_n and x_{n+1}. 
-
+    
         # Compute log p(x|theta) = log p(x|x0, theta) + log p(x0|theta)
         ll = euler_maruyama_state_sample_object.log_prob(C_PATH[:, 1:, :]).sum(-1) # log p(x|x0, theta)
-        ll += INIT_PRIOR.log_prob(C_PATH[:, 0, :]) # log p(x0|theta)
+        if LIDX == 0:
+            ll += INIT_PRIOR.log_prob(C_PATH[:, 0, :]) # log p(x0|theta)
+    elif LIK_DIST == 'TruncatedNormal':
+        drift, diffusion_sqrt, x_add_CO2 = SBM_SDE_CLASS.drift_diffusion_add_CO2(C_PATH, PARAMS_DICT, LIDX, RIDX, diffusion_matrix=False)
+        euler_maruyama_state_sample_object = TruncatedNormal(loc = C_PATH[:, :-1, :] + drift * DT, scale = diffusion_sqrt * math.sqrt(DT), a = 0, b = float('inf'))
 
-        return ll, drift, diffusion_sqrt
+        # Compute log p(x|theta) = log p(x|x0, theta) + log p(x0|theta)
+        ll = euler_maruyama_state_sample_object.log_prob(C_PATH[:, 1:, :]).sum((-2, -1)) # log p(x|x0, theta)
+        if LIDX == 0:
+            ll += INIT_PRIOR.log_prob(C_PATH[:, 0, :]).sum(-1) # log p(x0|theta)        
+
+    return ll, drift, diffusion_sqrt, x_add_CO2
+    #else:
+    #    drift, diffusion_sqrt = SBM_SDE_CLASS.drift_diffusion(C_PATH, PARAMS_DICT) #Appropriate indexing of tensors corresponding to data generating process now handled in `drift_diffusion` class method. Recall that drift diffusion will use C_PATH[:, :-1, :], I_S_TENSOR[:, 1:, :], I_D_TENSOR[:, 1:, :], TEMP_TENSOR[:, 1:, :]. 
+    #    euler_maruyama_state_sample_object = D.multivariate_normal.MultivariateNormal(loc = C_PATH[:, :-1, :] + drift * DT, scale_tril = diffusion_sqrt * math.sqrt(DT)) #C_PATH[:, :-1, :] + drift * DT will diverge from C_PATH if C_PATH values not compatible with x0 and theta. Algorithm aims to minimize gap between computed drift and actual gradient between x_n and x_{n+1}. 
+    #
+    #    # Compute log p(x|theta) = log p(x|x0, theta) + log p(x0|theta)
+    #    ll = euler_maruyama_state_sample_object.log_prob(C_PATH[:, 1:, :]).sum(-1) # log p(x|x0, theta)
+    #    ll += INIT_PRIOR.log_prob(C_PATH[:, 0, :]) # log p(x0|theta)
+    #
+    #    return ll, drift, diffusion_sqrt
 
 def train1(DEVICE, ELBO_LR, NITER, BATCH_SIZE, NUM_LAYERS,
         OBS_CSV_STR, OBS_ERROR_SCALE, T, DT, N,
         T_SPAN_TENSOR, I_S_TENSOR, I_D_TENSOR, TEMP_TENSOR, TEMP_REF,
         SBM_SDE_CLASS: str, DIFFUSION_TYPE: str,
         INIT_PRIOR, PRIOR_DIST_DETAILS_DICT, FIX_THETA_DICT = None, LEARN_CO2: bool = False,
-        THETA_DIST = None, THETA_POST_DIST = None, THETA_POST_INIT = None, TRANS_DIST = None,
+        THETA_DIST = None, THETA_POST_DIST = None, THETA_POST_INIT = None, LIK_DIST = None,
         BYPASS_NAN: bool = False, LR_DECAY: float = 0.8, DECAY_STEP_SIZE: int = 50000, PRINT_EVERY: int = 100,
-        DEBUG_SAVE_DIR: str = None):
+        DEBUG_SAVE_DIR: str = None, MINIBATCH_SIZE: int = 0):
 
     # if PRETRAIN_ITER >= NITER:
     #     raise ValueError('PRETRAIN_ITER must be < NITER.')
@@ -159,26 +165,21 @@ def train1(DEVICE, ELBO_LR, NITER, BATCH_SIZE, NUM_LAYERS,
     ELBO_params = list(net.parameters()) + list(q_theta.parameters())
     ELBO_optimizer = optim.Adamax(ELBO_params, lr = ELBO_LR)
     
+    # Sample minibatch indices
+    if 0 < MINIBATCH_SIZE < N:
+        minibatch_indices = torch.arange(0, N - minibatch_size, minibatch_size) + 1
+        rand = torch.randint(len(minibatch_indices), (NITER, ))
+        assert torch.min(torch.bincount(rand)) > 0 # verify that each minibatch is used at least once
+        batch_indices = MINIBATCH_INDICES[rand]
+    else:
+        # If MINIBATCH_SIZE is outside of acceptable range, then use full batch by default
+        batch_indices = None
+
     #Training loop
     # if BYPASS_NAN:
     #     torch.autograd.set_detect_anomaly(True)
     with tqdm(total = NITER, desc = f'Learning SDE and hidden parameters.', position = -1) as tq:
         for it in range(NITER):
-            net.train()
-            C_PATH, log_prob = net(BATCH_SIZE) #Obtain paths with solutions to times including t0.
-            
-            #NaN handling            
-            nan_count = 0
-            #Check for NaNs in x.
-            if torch.isnan(C_PATH).any():
-                if BYPASS_NAN:
-                    nan_count += 1
-                    print(f'nan_count = {nan_count}')
-                    print(f'Warning. NaN in x at niter: {it}. Using `torch.nan_to_num` to bypass. Check gradient clipping and learning rate to start.')
-                    C_PATH = torch.nan_to_num(C_PATH)
-                else:
-                    raise ValueError(f'nan in x at niter: {it}. Check gradient clipping and learning rate to start.')
-            
             # if it <= PRETRAIN_ITER:
             #     pretrain_optimizer.zero_grad()
             #
@@ -202,6 +203,7 @@ def train1(DEVICE, ELBO_LR, NITER, BATCH_SIZE, NUM_LAYERS,
             #     pretrain_optimizer.step()
 
             # else:
+            net.train()
             ELBO_optimizer.zero_grad()                
             
             list_theta = []
@@ -218,13 +220,34 @@ def train1(DEVICE, ELBO_LR, NITER, BATCH_SIZE, NUM_LAYERS,
             if FIX_THETA_DICT:
                 theta_dict = {**theta_dict, **FIX_THETA_DICT}
 
-            log_lik, drift, diffusion_sqrt = calc_log_lik1(C_PATH, theta_dict, DT, SBM_SDE, INIT_PRIOR)
+            # Sample x_{u-1:v} (unless u = 0, then sample x_{u:v})
+            if batch_indices is not None:
+                lidx = max(0, batch_indices[it] - 1)              # u-1 if u > 0, else 0
+                ridx = min(N, batch_indices[it] + MINIBATCH_SIZE) # v
+            else:
+                lidx, ridx = 0, N
+            C_PATH, log_prob = net(BATCH_SIZE, lidx, ridx, theta=theta_dict) #Obtain paths with solutions to times including t0.
+            
+            #NaN handling            
+            nan_count = 0
+            #Check for NaNs in x.
+            if torch.isnan(C_PATH).any():
+                if BYPASS_NAN:
+                    nan_count += 1
+                    print(f'nan_count = {nan_count}')
+                    print(f'Warning. NaN in x at niter: {it}. Using `torch.nan_to_num` to bypass. Check gradient clipping and learning rate to start.')
+                    C_PATH = torch.nan_to_num(C_PATH)
+                else:
+                    raise ValueError(f'nan in x at niter: {it}. Check gradient clipping and learning rate to start.')
+            
+            # Compute likelihood
+            log_lik, drift, diffusion_sqrt = calc_log_lik(C_PATH, theta_dict, DT, SBM_SDE, INIT_PRIOR, lidx, ridx)
 
             if LEARN_CO2:
-                x_add_CO2 = SBM_SDE.add_CO2(C_PATH, theta_dict)
-                ELBO = -log_p_theta.mean() + log_q_theta.mean() + log_prob.mean() - log_lik.mean() - obs_model(x_add_CO2, theta_dict)
+                x_add_CO2 = SBM_SDE.add_CO2(C_PATH, theta_dict, lidx, ridx)
+                ELBO = -log_p_theta.mean() + log_q_theta.mean() + log_prob.mean() - log_lik.mean() - obs_model(x_add_CO2, theta_dict, lidx, ridx)
             else:
-                ELBO = -log_p_theta.mean() + log_q_theta.mean() + log_prob.mean() - log_lik.mean() - obs_model(C_PATH, theta_dict)
+                ELBO = -log_p_theta.mean() + log_q_theta.mean() + log_prob.mean() - log_lik.mean() - obs_model(C_PATH, theta_dict, lidx, ridx)
 
             #Negative ELBO: -log p(theta) + log q(theta) - log p(y_0|x_0, theta) [already accounted for in obs_model output when learning x_0] + log q(x|theta) - log p(x|theta) - log p(y|x, theta)
             best_loss_ELBO = ELBO if ELBO < best_loss_ELBO else best_loss_ELBO
@@ -266,9 +289,9 @@ def train2(DEVICE, ELBO_LR, NITER, BATCH_SIZE, NUM_LAYERS,
         T_SPAN_TENSOR, I_S_TENSOR, I_D_TENSOR, TEMP_TENSOR, TEMP_REF,
         SBM_SDE_CLASS: str, DIFFUSION_TYPE: str,
         INIT_PRIOR, PRIOR_DIST_DETAILS_DICT, FIX_THETA_DICT = None, LEARN_CO2: bool = False,
-        THETA_DIST = None, THETA_POST_DIST = None, THETA_POST_INIT = None, TRANS_DIST = None,
+        THETA_DIST = None, THETA_POST_DIST = None, THETA_POST_INIT = None, LIK_DIST = None,
         BYPASS_NAN: bool = False, LR_DECAY: float = 0.8, DECAY_STEP_SIZE: int = 50000, PRINT_EVERY: int = 100,
-        DEBUG_SAVE_DIR: str = None):
+        DEBUG_SAVE_DIR: str = None, MINIBATCH_SIZE: int = 0):
 
     # if PRETRAIN_ITER >= NITER:
     #     raise ValueError('PRETRAIN_ITER must be < NITER.')
@@ -334,27 +357,22 @@ def train2(DEVICE, ELBO_LR, NITER, BATCH_SIZE, NUM_LAYERS,
     #pretrain_optimizer = optim.Adam(net.parameters(), lr = PRETRAIN_LR)
     ELBO_params = list(net.parameters()) + list(q_theta.parameters())
     ELBO_optimizer = optim.Adamax(ELBO_params, lr = ELBO_LR)
+
+    # Sample minibatch indices
+    if 0 < MINIBATCH_SIZE < N:
+        minibatch_indices = torch.arange(0, N - minibatch_size, minibatch_size) + 1
+        rand = torch.randint(len(minibatch_indices), (NITER, ))
+        assert torch.min(torch.bincount(rand)) > 0 # verify that each minibatch is used at least once
+        batch_indices = MINIBATCH_INDICES[rand]
+    else:
+        # If MINIBATCH_SIZE is outside of acceptable range, then use full batch by default
+        batch_indices = None
     
     #Training loop
     # if BYPASS_NAN:
     #         torch.autograd.set_detect_anomaly(True)
     with tqdm(total = NITER, desc = f'Learning SDE and hidden parameters.', position = -1) as tq:
         for it in range(NITER):
-            net.train()
-            C_PATH, log_prob = net(BATCH_SIZE) #Obtain paths with solutions to times including t0.
-            
-            #NaN handling            
-            nan_count = 0
-            #Check for NaNs in x.
-            if torch.isnan(C_PATH).any():
-                if BYPASS_NAN:
-                    nan_count += 1
-                    print(f'nan_count = {nan_count}')
-                    print(f'Warning. NaN in x at niter: {it}. Using `torch.nan_to_num` to bypass. Check gradient clipping and learning rate to start.')
-                    C_PATH = torch.nan_to_num(C_PATH)
-                else:
-                    raise ValueError(f'nan in x at niter: {it}. Check gradient clipping and learning rate to start.')
-            
             # if it <= PRETRAIN_ITER:
             #     pretrain_optimizer.zero_grad()
             #
@@ -378,6 +396,7 @@ def train2(DEVICE, ELBO_LR, NITER, BATCH_SIZE, NUM_LAYERS,
             #     pretrain_optimizer.step()
 
             # else:
+            net.train()
             ELBO_optimizer.zero_grad()                
             
             list_theta = []
@@ -394,12 +413,33 @@ def train2(DEVICE, ELBO_LR, NITER, BATCH_SIZE, NUM_LAYERS,
             if FIX_THETA_DICT:
                 theta_dict = {**theta_dict, **FIX_THETA_DICT}
 
-            if LEARN_CO2:
-                log_lik, drift, diffusion_sqrt, x_add_CO2 = calc_log_lik2(C_PATH, theta_dict, DT, SBM_SDE, INIT_PRIOR, LEARN_CO2)
-                ELBO = -log_p_theta.mean() + log_q_theta.mean() + log_prob.mean() - log_lik.mean() - obs_model(x_add_CO2, theta_dict)
+            # Sample x_{u-1:v} (unless u = 0, then sample x_{u:v})
+            if batch_indices is not None:
+                lidx = max(0, batch_indices[it] - 1)              # u-1 if u > 0, else 0
+                ridx = min(N, batch_indices[it] + MINIBATCH_SIZE) # v
             else:
-                log_lik, drift, diffusion_sqrt = calc_log_lik2(C_PATH, theta_dict, DT, SBM_SDE, INIT_PRIOR, LEARN_CO2)
-                ELBO = -log_p_theta.mean() + log_q_theta.mean() + log_prob.mean() - log_lik.mean() - obs_model(C_PATH, theta_dict)
+                lidx, ridx = 0, N
+            C_PATH, log_prob = net(BATCH_SIZE) #Obtain paths with solutions to times including t0.
+            
+            #NaN handling            
+            nan_count = 0
+            #Check for NaNs in x.
+            if torch.isnan(C_PATH).any():
+                if BYPASS_NAN:
+                    nan_count += 1
+                    print(f'nan_count = {nan_count}')
+                    print(f'Warning. NaN in x at niter: {it}. Using `torch.nan_to_num` to bypass. Check gradient clipping and learning rate to start.')
+                    C_PATH = torch.nan_to_num(C_PATH)
+                else:
+                    raise ValueError(f'nan in x at niter: {it}. Check gradient clipping and learning rate to start.')
+
+            # Compute likelihood
+            if LEARN_CO2:
+                log_lik, drift, diffusion_sqrt, x_add_CO2 = calc_log_lik_CO2(C_PATH, theta_dict, DT, SBM_SDE, INIT_PRIOR, lidx, ridx)
+                ELBO = -log_p_theta.mean() + log_q_theta.mean() + log_prob.mean() - log_lik.mean() - obs_model(x_add_CO2, theta_dict, lidx, ridx)
+            else:
+                log_lik, drift, diffusion_sqrt = calc_log_lik(C_PATH, theta_dict, DT, SBM_SDE, INIT_PRIOR, lidx, ridx)
+                ELBO = -log_p_theta.mean() + log_q_theta.mean() + log_prob.mean() - log_lik.mean() - obs_model(C_PATH, theta_dict, lidx, ridx)
 
             #Negative ELBO: -log p(theta) + log q(theta) - log p(y_0|x_0, theta) [already accounted for in obs_model output when learning x_0] + log q(x|theta) - log p(x|theta) - log p(y|x, theta)
             best_loss_ELBO = ELBO if ELBO < best_loss_ELBO else best_loss_ELBO
