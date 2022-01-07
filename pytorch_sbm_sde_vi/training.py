28f314faf737cadcd02b3b30eef6a07b6986f8c1
Hua Xie <x@wallyxie.com>
2022-01-06 19:02:17 -0800

    Remove need to wastefully compute `mean_state_obs` in every iteration of pretraining. Now computed once before training loop. Removed train1 from old training module. Added train_NN_minibatch for testing fixed NN inference without conditioning on observations.

--- pytorch_sbm_sde_vi/training.py
+++ pytorch_sbm_sde_vi/training.py
@@ -13,7 +13,7 @@ import torch.nn.functional as F
 import torch.optim as optim
 
 #Module imports
-from mean_field import *
+from mean_field_smooth import *
 from obs_and_flow import *
 from SBM_SDE_classes import *
 
@@ -71,180 +71,6 @@ def calc_log_lik2(C_PATH: torch.Tensor,
 
         return ll, drift, diffusion_sqrt
 
-def train1(DEVICE, ELBO_LR, N_ITER, BATCH_SIZE, NUM_LAYERS,
-        OBS_CSV_STR, OBS_ERROR_SCALE, T, DT, N,
-        T_SPAN_TENSOR, I_S_TENSOR, I_D_TENSOR, TEMP_TENSOR, TEMP_REF,
-        SBM_SDE_CLASS: str, DIFFUSION_TYPE: str,
-        INIT_PRIOR, PRIOR_DIST_DETAILS_DICT, FIX_THETA_DICT = None, LEARN_CO2: bool = False,
-        THETA_DIST = None, THETA_POST_DIST = None, THETA_POST_INIT = None,
-        BYPASS_NAN: bool = False, LR_DECAY: float = 0.8, DECAY_STEP_SIZE: int = 50000, PRINT_EVERY: int = 100,
-        DEBUG_SAVE_DIR: str = None):
-
-    # if PRETRAIN_ITER >= N_ITER:
-    #     raise ValueError('PRETRAIN_ITER must be < N_ITER.')
-
-    #Instantiate SBM_SDE object based on specified model and diffusion type.
-    SBM_SDE_class_dict = {
-            'SCON': SCON,
-            'SAWB': SAWB,
-            'SAWB-ECA': SAWB_ECA
-            }
-    if SBM_SDE_CLASS not in SBM_SDE_class_dict:
-        raise NotImplementedError('Other SBM SDEs aside from SCON, SAWB, and SAWB-ECA have not been implemented yet.')
-    SBM_SDE_class = SBM_SDE_class_dict[SBM_SDE_CLASS]
-    SBM_SDE = SBM_SDE_class(T_SPAN_TENSOR, I_S_TENSOR, I_D_TENSOR, TEMP_TENSOR, TEMP_REF, DIFFUSION_TYPE)
-
-    #Read in data to obtain y and establish observation model.
-    obs_dim = None
-    if LEARN_CO2:
-        obs_dim = SBM_SDE.state_dim + 1
-    else:
-        obs_dim = SBM_SDE.state_dim
-    obs_times, obs_means, obs_error = csv_to_obs_df(OBS_CSV_STR, obs_dim, T, OBS_ERROR_SCALE) #csv_to_obs_df function in obs_and_flow module
-    obs_model = ObsModel(DEVICE, TIMES = obs_times, DT = DT, MU = obs_means, SCALE = obs_error).to(DEVICE)
-
-    #Establish neural network.
-    net = SDEFlow(DEVICE, obs_model, SBM_SDE.state_dim, T, DT, N, num_layers = NUM_LAYERS).to(DEVICE)
-
-    #Initiate model debugging saver.
-    if DEBUG_SAVE_DIR:
-        debug_saver = ModelSaver(save_dir = DEBUG_SAVE_DIR)
-
-    param_names = list(PRIOR_DIST_DETAILS_DICT.keys())
-
-    #Convert prior details dictionary values to tensors.
-    prior_list = list(zip(*(PRIOR_DIST_DETAILS_DICT[k] for k in param_names))) #Unzip prior distribution details from dictionary values into individual lists.
-    prior_means_tensor, prior_sds_tensor, prior_lowers_tensor, prior_uppers_tensor = torch.tensor(prior_list).to(DEVICE) #Ensure conversion of lists into tensors.
-
-    #Retrieve desired distribution class based on string.
-    dist_class_dict = {
-            'TruncatedNormal': TruncatedNormal,
-            'RescaledLogitNormal': RescaledLogitNormal,
-            'MultivariateLogitNormal': MultivariateLogitNormal
-            }
-    THETA_PRIOR_CLASS = dist_class_dict[THETA_DIST]
-    THETA_POST_CLASS = dist_class_dict[THETA_POST_DIST] if THETA_POST_DIST else dist_class_dict[THETA_DIST]
-
-    #Define prior
-    priors = THETA_PRIOR_CLASS(loc = prior_means_tensor, scale = prior_sds_tensor, a = prior_lowers_tensor, b = prior_uppers_tensor)
-
-    # Initialize posterior q(theta) using its prior p(theta)
-    learn_cov = (THETA_POST_DIST == 'MultivariateLogitNormal')
-    if THETA_POST_INIT is None:
-        THETA_POST_INIT = PRIOR_DIST_DETAILS_DICT
-    q_theta = MeanField(DEVICE, param_names, THETA_POST_INIT, THETA_POST_CLASS, learn_cov)
-
-    #Record loss throughout training
-    best_loss_norm = 1e15
-    best_loss_ELBO = 1e15
-    norm_losses = []
-    ELBO_losses = []
-
-    #Initiate optimizers.
-    pretrain_optimizer = optim.Adam(net.parameters(), lr = PTRAIN_LR)
-    ELBO_params = list(net.parameters()) + list(q_theta.parameters())
-    ELBO_optimizer = optim.Adamax(ELBO_params, lr = ELBO_LR)
-
-    #Training loop
-    with tqdm(total = N_ITER, desc = f'Learning SDE and hidden parameters.', position = -1) as tq:
-        for it in range(N_ITER):
-            net.train()
-            C_PATH, log_prob = net(BATCH_SIZE) #Obtain paths with solutions to times including t0.
-
-            #NaN handling            
-            nan_count = 0
-            #Check for NaNs in x.
-            if torch.isnan(C_PATH).any():
-                if BYPASS_NAN:
-                    nan_count += 1
-                    print(f'nan_count = {nan_count}')
-                    print(f'Warning. NaN in x at niter: {it}. Using `torch.nan_to_num` to bypass. Check gradient clipping and learning rate to start.')
-                    C_PATH = torch.nan_to_num(C_PATH)
-                else:
-                    raise ValueError(f'nan in x at niter: {it}. Check gradient clipping and learning rate to start.')
-
-            if it < PTRAIN_ITER:
-                pretrain_optimizer.zero_grad()
-
-                if LEARN_CO2:
-                    mean_state_obs = torch.mean(obs_model.mu[:-1, :], -1)[None, None, :]
-                else:
-                    mean_state_obs = torch.mean(obs_model.mu, -1)[None, None, :]
-
-                if PTRAIN_ALG == 'L1':
-                    l1_norm_element = C_PATH - mean_state_obs #Compute difference between x and observed state means.
-                    norm = torch.sum(torch.abs(l1_norm_element), (-1, -2)).mean() #Take L1 mean across all samples.
-                    best_loss_norm = norm if norm < best_loss_norm else best_loss_norm
-                    norm_losses.append(norm.item())
-
-                elif PTRAIN_ALG == 'L2':
-                    l2_norm_element = C_PATH - mean_state_obs #Compute difference between x and observed state means.
-                    norm = torch.sqrt(torch.sum(torch.square(l2_norm_element), (-1, -2))).mean() #Take L2 mean across all samples.
-                    best_loss_norm = norm if norm < best_loss_norm else best_loss_norm
-                    norm_losses.append(norm.item())
-
-                if (it + 1) % PRINT_EVERY == 0:
-                    print(f'\nMoving average norm loss at {it + 1} iterations is: {sum(norm_losses[-10:]) / len(norm_losses[-10:])}. Best norm loss value is: {best_loss_norm}.')
-                    print('\nC_PATH mean =', C_PATH.mean(-2))
-                    print('\nC_PATH =', C_PATH)
-
-                norm.backward()
-                torch.nn.utils.clip_grad_norm_(net.parameters(), 3.0)                                
-                pretrain_optimizer.step()
-
-            else:
-                ELBO_optimizer.zero_grad()                
-
-                theta_dict, theta, log_q_theta, parent_loc_scale_dict = q_theta(BATCH_SIZE)
-                log_p_theta = priors.log_prob(theta).sum(-1)
-
-                if FIX_THETA_DICT:
-                    if platform.python_version() >= '3.9.0':
-                        theta_dict = theta_dict | FIX_THETA_DICT
-                    else:
-                        theta_dict = {**theta_dict, **FIX_THETA_DICT}
-
-                log_lik, drift, diffusion_sqrt = calc_log_lik1(C_PATH, theta_dict, DT, SBM_SDE, INIT_PRIOR)
-
-                if LEARN_CO2:
-                    x_add_CO2 = SBM_SDE.add_CO2(C_PATH, theta_dict)
-                    ELBO = -log_p_theta.mean() + log_q_theta.mean() + log_prob.mean() - log_lik.mean() - obs_model(x_add_CO2, theta_dict)
-                else:
-                    ELBO = -log_p_theta.mean() + log_q_theta.mean() + log_prob.mean() - log_lik.mean() - obs_model(C_PATH, theta_dict)
-
-                #Negative ELBO: -log p(theta) + log q(theta) - log p(y_0|x_0, theta) [already accounted for in obs_model output when learning x_0] + log q(x|theta) - log p(x|theta) - log p(y|x, theta)
-                best_loss_ELBO = ELBO if ELBO < best_loss_ELBO else best_loss_ELBO
-                ELBO_losses.append(ELBO.item())
-
-                if (it + 1) % PRINT_EVERY == 0:
-                    print(f'\ndrift at {it + 1} iterations: {drift}')
-                    print(f'\ndiffusion_sqrt at {it + 1} iterations = {diffusion_sqrt}')
-                    print(f'\nMoving average ELBO loss at {it + 1} iterations is: {sum(ELBO_losses[-10:]) / len(ELBO_losses[-10:])}. Best ELBO loss value is: {best_loss_ELBO}.')
-                    if LEARN_CO2:
-                        print('\nC_PATH with CO2 mean =', x_add_CO2.mean(-2))
-                        print('\nC_PATH with CO2 =', x_add_CO2)
-                    else:
-                        print('\nC_PATH mean =', C_PATH.mean(-2))
-                        print('\nC_PATH =', C_PATH)
-                    print('\ntheta_dict means: ', {key: theta_dict[key].mean() for key in param_names})
-                    print('\nparent_loc_scale_dict: ', parent_loc_scale_dict)
-
-                ELBO.backward()
-                torch.nn.utils.clip_grad_norm_(ELBO_params, 5.0)
-                ELBO_optimizer.step()
-
-                if it % DECAY_STEP_SIZE == 0:
-                    ELBO_optimizer.param_groups[0]['lr'] *= LR_DECAY
-
-            if DEBUG_SAVE_DIR:
-                to_save = {'model': net, 'model_state_dict': net.state_dict(), 'optimizer_state_dict': ELBO_optimizer.state_dict(),
-                        'q_theta': q_theta}
-                debug_saver.save(to_save, it + 1)
-
-            tq.update()
-
-    return net, q_theta, priors, obs_model, ELBO_losses, SBM_SDE
-
 def train2(DEVICE, ELBO_LR, N_ITER, BATCH_SIZE, NUM_LAYERS,
         OBS_CSV_STR, OBS_ERROR_SCALE, T, DT, N,
         T_SPAN_TENSOR, I_S_TENSOR, I_D_TENSOR, TEMP_TENSOR, TEMP_REF,
@@ -276,6 +102,11 @@ def train2(DEVICE, ELBO_LR, N_ITER, BATCH_SIZE, NUM_LAYERS,
         obs_dim = SBM_SDE.state_dim
     obs_times, obs_means, obs_error = csv_to_obs_df(OBS_CSV_STR, obs_dim, T, OBS_ERROR_SCALE) #csv_to_obs_df function in obs_and_flow module
     obs_model = ObsModel(DEVICE, TIMES = obs_times, DT = DT, MU = obs_means, SCALE = obs_error).to(DEVICE)
+
+    if LEARN_CO2 and PTRAIN_ITER != 0 and PTRAIN_ALG:
+        mean_state_obs = torch.mean(obs_model.mu[:-1, :], -1)[None, None, :]
+    elif not LEARN_CO2 and PTRAIN_ITER != 0 and PTRAIN_ALG:
+        mean_state_obs = torch.mean(obs_model.mu, -1)[None, None, :]
 
     #Establish neural network.
     net = SDEFlow(DEVICE, obs_model, SBM_SDE.state_dim, T, DT, N, num_layers = NUM_LAYERS).to(DEVICE)
@@ -340,11 +171,6 @@ def train2(DEVICE, ELBO_LR, N_ITER, BATCH_SIZE, NUM_LAYERS,
             
             if it <= PTRAIN_ITER:
                 pretrain_optimizer.zero_grad()
-
-                if LEARN_CO2:
-                    mean_state_obs = torch.mean(obs_model.mu[:-1, :], -1)[None, None, :]
-                else:
-                    mean_state_obs = torch.mean(obs_model.mu, -1)[None, None, :]
 
                 if PTRAIN_ALG == 'L1':
                     l1_norm_element = C_PATH - mean_state_obs #Compute difference between x and observed state means.
