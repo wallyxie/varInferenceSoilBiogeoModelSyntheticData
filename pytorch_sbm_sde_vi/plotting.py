c244086d27ca46058efeb6f59f42dc5a843e4f19
Hua Xie <x@wallyxie.com>
2022-01-09 08:07:34 -0800

    Added GPU learn NN script corresponding to old architecture.

--- pytorch_sbm_sde_vi/plotting.py
+++ pytorch_sbm_sde_vi/plotting.py
@@ -70,6 +70,49 @@ def plot_states_post(x, q_theta, obs_model, SBM_SDE_CLASS, niter, piter, t, dt, 
     fig.set_size_inches(15, 15)
     fig.savefig(os.path.join(plots_folder, f'net_iter_{niter}_piter_{piter}_t_{t}_dt_{dt}_batch_{batch_size}_samples_{eval_batch_size}_layers_{num_layers}_lr_{train_lr}_sd_scale_{sd_scale}_{now_string}.png'), dpi = 300)
 
+def plot_states_NN(x, params_dict, obs_model, SBM_SDE_CLASS, niter, piter, t, dt, batch_size, eval_batch_size, num_layers, train_lr, sd_scale, plots_folder, now_string, LEARN_CO2 = False, ymin_list = None, ymax_list = None):
+
+    state_list = []
+
+    if x.size(-1) == 3 and not LEARN_CO2:
+        state_list = ['SOC', 'DOC', 'MBC']
+    elif x.size(-1) == 3 and LEARN_CO2:
+        state_list = ['SOC', 'DOC', 'MBC', 'CO2']
+    elif x.size(-1) == 4 and not LEARN_CO2:
+        state_list = ['SOC', 'DOC', 'MBC', 'EEC']
+    elif x.size(-1) == 4 and LEARN_CO2:
+        state_list = ['SOC', 'DOC', 'MBC', 'EEC', 'CO2']
+    else:
+        raise Exception('Matching condition does not exist with x.size() and LEARN_CO2 status.')
+
+    if LEARN_CO2:
+        x = SBM_SDE_CLASS.add_CO2(x, params_dict) #Add CO2 to x tensor if CO2 is being fit.
+
+    fig, axs = plt.subplots(x.size(-1))
+
+    obs_model.mu = obs_model.mu.detach().cpu().numpy()
+    obs_model.scale = obs_model.scale.detach().cpu().numpy()
+
+    for i in range(x.size(-1)):
+        q_mean, q_std = x[:, :, i].mean(0).detach().cpu().numpy(), x[:, :, i].std(0).detach().cpu().numpy()
+        hours = torch.arange(0, t + dt, dt).detach().cpu().numpy()
+        axs[i].plot(obs_model.times, obs_model.mu[i, :], linestyle = 'None', marker = '.', label = 'Observed')
+        axs[i].fill_between(obs_model.times, obs_model.mu[i, :] - 2 * obs_model.scale[:, i], obs_model.mu[i, :] + 2 * obs_model.scale[:, i], alpha = 0.4, label = 'Observation $\\mu \pm 2\sigma_y$')
+        axs[i].plot(hours, q_mean, label = 'Posterior mean')
+        axs[i].fill_between(hours, q_mean - 2 * q_std, q_mean + 2 * q_std, alpha = 0.4, label = 'Posterior $\\mu \pm 2\sigma_x$')
+        state = state_list[i]
+        #axs[i].legend()
+        plt.setp(axs[i], ylabel = state)
+        ymin = ymin_list[i] if ymin_list else None
+        ymax = ymax_list[i] if ymax_list else None
+        axs[i].set_ylim([ymin, ymax])
+        #plt.title(f'Approximate posterior $q(x|\\theta, y)$\nNumber of samples = {eval_batch_size}\nTimestep = {dt}\nIterations = {niter}')
+    plt.xlabel('Hour')
+    #plt.tight_layout()
+    fig.set_size_inches(15, 15)
+    fig.savefig(os.path.join(plots_folder, f'net_iter_{niter}_piter_{piter}_t_{t}_dt_{dt}_batch_{batch_size}_samples_{eval_batch_size}_layers_{num_layers}_lr_{train_lr}_sd_scale_{sd_scale}_{now_string}.png'), dpi = 300)
+
+
 def plot_theta(p_theta, q_theta, true_theta, niter, piter, t, dt, batch_size, eval_batch_size, num_layers, train_lr, sd_scale, plots_folder, now_string, ncols=4):
     # Prior distribution object
     p_dist = p_theta
