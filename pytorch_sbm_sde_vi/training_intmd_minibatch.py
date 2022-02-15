75d0b904b760fb183f3b37d067b2119515154637
Hua Xie <x@wallyxie.com>
2022-02-14 16:26:18 -0800

    log_prob.sum(-1).mean() --> log_prob.mean() for log q(x | theta) ELBO computation.

--- pytorch_sbm_sde_vi/training_intmd_minibatch.py
+++ pytorch_sbm_sde_vi/training_intmd_minibatch.py
@@ -270,10 +270,10 @@ def train_minibatch(DEVICE, ELBO_LR: float, ELBO_ITER: int, BATCH_SIZE: int,
                 # Negative ELBO: -log p(theta) + log q(theta) - log p(y_0|x_0, theta) [already accounted for in obs_model output when learning x_0] + log q(x|theta) - log p(x|theta) - log p(y|x, theta)
                 if LEARN_CO2:
                     log_lik, drift, diffusion_sqrt, x_add_CO2 = calc_log_lik_minibatch_CO2(C_PATH, theta_dict, DT, SBM_SDE, INIT_PRIOR, lidx, ridx, LIK_DIST)
-                    ELBO = -log_p_theta.mean() + log_q_theta.mean() + N/minibatch_size * (log_prob.sum(-1).mean() - log_lik.mean() - obs_model_minibatch(x_add_CO2, theta_dict, lidx, ridx))
+                    ELBO = -log_p_theta.mean() + log_q_theta.mean() + N/minibatch_size * (log_prob.mean() - log_lik.mean() - obs_model_minibatch(x_add_CO2, theta_dict, lidx, ridx))
                 else:
                     log_lik, drift, diffusion_sqrt = calc_log_lik_minibatch(C_PATH, theta_dict, DT, SBM_SDE, INIT_PRIOR, lidx, ridx, LIK_DIST)
-                    ELBO = -log_p_theta.mean() + log_q_theta.mean() + N/minibatch_size * (log_prob.sum(-1).mean() - log_lik.mean() - obs_model_minibatch(C_PATH, theta_dict, lidx, ridx))
+                    ELBO = -log_p_theta.mean() + log_q_theta.mean() + N/minibatch_size * (log_prob.mean() - log_lik.mean() - obs_model_minibatch(C_PATH, theta_dict, lidx, ridx))
 
                 #Negative ELBO: -log p(theta) + log q(theta) - log p(y_0|x_0, theta) [already accounted for in obs_model output when learning x_0] + log q(x|theta) - log p(x|theta) - log p(y|x, theta)
                 best_loss_ELBO = ELBO if ELBO < best_loss_ELBO else best_loss_ELBO
@@ -459,10 +459,10 @@ def train_nn_minibatch(DEVICE, ELBO_LR: float, ELBO_ITER: int, BATCH_SIZE: int,
                 # Negative ELBO: -log p(theta) + log q(theta) - log p(y_0|x_0, theta) [already accounted for in obs_model output when learning x_0] + log q(x|theta) - log p(x|theta) - log p(y|x, theta)
                 if LEARN_CO2:
                     log_lik, drift, diffusion_sqrt, x_add_CO2 = calc_log_lik_minibatch_CO2(C_PATH, theta_dict, DT, SBM_SDE, INIT_PRIOR, lidx, ridx, LIK_DIST)
-                    ELBO = N/minibatch_size * (log_prob.sum(-1).mean() - log_lik.mean() - obs_model_minibatch(x_add_CO2, theta_dict, lidx, ridx))
+                    ELBO = N/minibatch_size * (log_prob.mean() - log_lik.mean() - obs_model_minibatch(x_add_CO2, theta_dict, lidx, ridx))
                 else:
                     log_lik, drift, diffusion_sqrt = calc_log_lik_minibatch(C_PATH, theta_dict, DT, SBM_SDE, INIT_PRIOR, lidx, ridx, LIK_DIST)
-                    ELBO = N/minibatch_size * (log_prob.sum(-1).mean() - log_lik.mean() - obs_model_minibatch(C_PATH, theta_dict, lidx, ridx))
+                    ELBO = N/minibatch_size * (log_prob.mean() - log_lik.mean() - obs_model_minibatch(C_PATH, theta_dict, lidx, ridx))
 
                 # Record ELBO history and best ELBO so far
                 best_loss_ELBO = ELBO if ELBO < best_loss_ELBO else best_loss_ELBO
