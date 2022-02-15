ce2f793148a8f7fe87ead15bcb224e895ccf2505
Hua Xie <x@wallyxie.com>
2022-02-15 12:22:59 -0800

    Switch intmd mini-batch to uni-batch mode.

--- pytorch_sbm_sde_vi/learn_theta_SCON-SS_CO2_intmd_minibatch_logit_short.py
+++ pytorch_sbm_sde_vi/learn_theta_SCON-SS_CO2_intmd_minibatch_logit_short.py
@@ -37,7 +37,7 @@ torch.manual_seed(0)
 #IAF SSM time parameters
 dt_flow = 1.0 #Increased from 0.1 to reduce memory.
 t = 5000 #In hours.
-minibatch_t = 1000
+minibatch_t = 5000
 n = int(t / dt_flow) + 1
 t_span = np.linspace(0, t, n)
 t_span_tensor = torch.reshape(torch.Tensor(t_span), [1, n, 1]).to(active_device) #T_span needs to be converted to tensor object. Additionally, facilitates conversion of I_S and I_D to tensor objects.
