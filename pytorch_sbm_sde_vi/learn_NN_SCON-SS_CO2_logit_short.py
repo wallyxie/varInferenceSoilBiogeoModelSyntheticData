e163bea9ae55572869f86aeb197f6c3f7928d6d1
Hua Xie <x@wallyxie.com>
2022-02-28 17:07:33 -0800

    Reduced ELBO iterations, reduced pre-decay LR, decreased LR decay step factor, reduced warmup iterations in response to fewer layer sets.

--- pytorch_sbm_sde_vi/learn_NN_SCON-SS_CO2_logit_short.py
+++ pytorch_sbm_sde_vi/learn_NN_SCON-SS_CO2_logit_short.py
@@ -49,11 +49,11 @@ temp_ref = 283
 temp_rise = 5 #High estimate of 5 celsius temperature rise by 2100.
 
 #Training parameters
-elbo_iter = 10
+elbo_iter = 100000
 elbo_lr = 1e-3
 elbo_lr_decay = 0.7
-elbo_lr_decay_step_size = 5000
-elbo_warmup_iter = 10
+elbo_lr_decay_step_size = 10000
+elbo_warmup_iter = 2000
 elbo_warmup_lr = 1e-6
 ptrain_iter = 0
 ptrain_alg = 'L1'
