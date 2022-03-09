dbd2a952cb1a042f3b7e3eacc09e7c71ea399843
Hua Xie <x@wallyxie.com>
2022-03-09 14:21:57 -0800

    Restored gen y trunc transitional sampling code.

--- pytorch_sbm_sde_vi/SCON_flow_GPU_sample.py
+++ pytorch_sbm_sde_vi/SCON_flow_GPU_sample.py
@@ -35,10 +35,10 @@ else:
 torch.set_printoptions(precision = 8)
 torch.manual_seed(0)
 
-now_string = 'SCON-C_CO2_logit_short_2022_03_08_12_39_02'
+now_string = 'SCON-C_CO2_logit_short_2022_03_08_10_09_11'
 outputs_folder = 'training_pt_outputs/'
 plots_folder = 'training_plots/'
-save_string = '_iter_110000_warmup_5000_t_5000_dt_1.0_batch_31_layers_5_lr_0.01_decay_step_15000_warmup_lr_1e-06_sd_scale_0.25_SCON-C_CO2_logit_short_2022_03_08_12_39_02.pt'
+save_string = '_iter_110000_warmup_5000_t_5000_dt_1.0_batch_73_layers_2_lr_0.01_decay_step_15000_warmup_lr_1e-06_sd_scale_0.25_SCON-C_CO2_logit_short_2022_03_08_10_09_11.pt'
 
 obs_model_save_string = os.path.join(outputs_folder, 'obs_model' + save_string)
 net_save_string = os.path.join(outputs_folder, 'net' + save_string)
