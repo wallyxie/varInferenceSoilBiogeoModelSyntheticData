Intermediate update of `log_prob` computation in batch renorm layer, and fixing of potential running mean/var/std update bug. Simplification of `ildj` computation accounting for use of std rather than var.

pytorch_sbm_sde_vi/learn_theta_SCON-C_CO2_logit_short_brn.py
@@ -133,7 +133,21 @@ print('Output files saving finished. Moving to plotting.')
 
 #Plot training posterior results and ELBO history.
 net.eval()
-x, _ = net(eval_batch_size)
+with torch.no_grad():
+    x, log_prob = net(eval_batch_size)
+    print('x = ', x)
+    theta_dict, theta, log_q_theta, parent_loc_scale_dict = q_theta(eval_batch_size)
+    log_p_theta = p_theta.log_prob(theta).sum(-1)
+    if fix_theta_dict:
+        if platform.python_version() >= '3.9.0':
+            theta_dict = theta_dict | fix_theta_dict
+        else:
+            theta_dict = {**theta_dict, **fix_theta_dict}
+    log_lik, drift, diffusion_sqrt, x_add_CO2 = calc_log_lik(x, theta_dict, dt_flow, SBM_SDE, x0_prior_SCON, learn_CO2)
+    neg_ELBO = -log_p_theta.mean() + log_q_theta.mean() + log_prob.mean() - log_lik.mean() - obs_model(x_add_CO2)
+    print('x.size() =', x.size())
+    print(f'Net with {train_args} has neg_ELBO = {neg_ELBO}')
+
 plots_folder = 'training_plots/'
 plot_elbo(ELBO_hist, elbo_iter, elbo_warmup_iter, t, dt_flow, batch_size, eval_batch_size, num_layers, elbo_lr, elbo_lr_decay_step_size, elbo_warmup_lr, prior_scale_factor, plots_folder, now_string, xmin = elbo_warmup_iter + int(elbo_iter / 2))
 print('ELBO plotting finished.')

pytorch_sbm_sde_vi/obs_and_flow_brn.py
@@ -228,11 +228,8 @@ class BatchRenormLayer(nn.Module):
             self.batch_mean = x.mean(0)
             self.batch_std = x.std(0, unbiased = False) + self.eps
 
-            self.running_mean.mul_(self.momentum)
-            self.running_std.mul_(self.momentum)
-
-            self.running_mean.add_(self.batch_mean.detach() * (1 - self.momentum))
-            self.running_std.add_(self.batch_std.detach() * (1 - self.momentum))
+            self.running_mean += self.momentum * (self.batch_mean.detach() - self.running_mean)
+            self.running_std += self.momentum * (self.batch_std.detach() - self.running_std)
 
             self.r_max = self.get_r_max(self.training_iter, self.batch_renorm_warmup_iter, self.init_r_max, self.max_r_max, self.r_max_step_size)
             self.d_max = self.get_d_max(self.training_iter, self.batch_renorm_warmup_iter, self.init_d_max, self.max_d_max, self.d_max_step_size)
@@ -258,7 +255,7 @@ class BatchRenormLayer(nn.Module):
             std = self.running_std
 
         y = torch.exp(self.log_gamma) * x_hat + self.beta # (batch_size, n * state_dim)
-        ildj = -self.log_gamma + 0.5 * torch.log(std.square()) # (n * state_dim, )
+        ildj = -self.log_gamma + torch.log(std) # (n * state_dim, )
 
         # y.shape == (batch_size, 1, n * state_dim), ildj.shape == (1, 1, n * state_dim)
         return y[:, None, :], ildj[None, None, :]