eea5c91fa577a2195a31da10d7f02d6b24898521
Wally Xie <x@wallyxie.com>
2023-08-22 14:39:43 -0700

    Update learn_NN_SCON-C_CO2_logit_short.py

--- pytorch_sbm_sde_vi/learn_NN_SCON-C_CO2_logit_short.py
+++ pytorch_sbm_sde_vi/learn_NN_SCON-C_CO2_logit_short.py
@@ -149,11 +149,12 @@ print(x_eval)
 x_eval_save_string = os.path.join(outputs_folder, 'x_eval' + save_string)
 torch.save(x_eval, x_eval_save_string)
 
-#Plot training posterior results and ELBO history (with lower training batch_size due to memory limitations).
-x, _ = net(batch_size)
+#Plot training posterior results and ELBO history.
+with torch.no_grad():
+    x, _ = net(eval_batch_size)
 plots_folder = 'training_plots/'
 plot_elbo(ELBO_hist, elbo_iter, elbo_warmup_iter, t, dt_flow, batch_size, eval_batch_size, num_layers, elbo_lr, elbo_lr_decay_step_size, elbo_warmup_lr, prior_scale_factor, plots_folder, now_string, xmin = elbo_warmup_iter + int(elbo_iter / 2))
 print('ELBO plotting finished.')
 params_dict_tensor = {k: torch.tensor(v).unsqueeze(0).to(active_device) for k, v in params_dict.items()}
-plot_states_NN(x, params_dict_tensor, obs_model, SBM_SDE_instance, elbo_iter, elbo_warmup_iter, t, dt_flow, batch_size, batch_size, num_layers, elbo_lr, elbo_lr_decay_step_size, elbo_warmup_lr, prior_scale_factor, plots_folder, now_string, learn_CO2, ymin_list = [0, 0, 0, 0], ymax_list = [70., 5., 8., 0.03])
+plot_states_NN(x, params_dict_tensor, obs_model, SBM_SDE_instance, elbo_iter, elbo_warmup_iter, t, dt_flow, batch_size, eval_batch_size, num_layers, elbo_lr, elbo_lr_decay_step_size, elbo_warmup_lr, prior_scale_factor, plots_folder, now_string, learn_CO2, ymin_list = [0, 0, 0, 0], ymax_list = [70., 5., 8., 0.03])
 print('States fit plotting finished.')
