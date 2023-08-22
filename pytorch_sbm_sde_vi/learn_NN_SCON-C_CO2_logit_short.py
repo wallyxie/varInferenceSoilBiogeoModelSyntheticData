e4f5fec5aab26be9f80d6b352ca7a566d3224eea
Wally Xie <x@wallyxie.com>
2023-08-22 14:22:46 -0700

    Update learn_NN_SCON-C_CO2_logit_short.py

--- pytorch_sbm_sde_vi/learn_NN_SCON-C_CO2_logit_short.py
+++ pytorch_sbm_sde_vi/learn_NN_SCON-C_CO2_logit_short.py
@@ -149,7 +149,7 @@ print(x_eval)
 x_eval_save_string = os.path.join(outputs_folder, 'x_eval' + save_string)
 torch.save(x_eval, x_eval_save_string)
 
-#Plot training posterior results and ELBO history (with lower training batch_size).
+#Plot training posterior results and ELBO history (with lower training batch_size due to memory limitations).
 x, _ = net(batch_size)
 plots_folder = 'training_plots/'
 plot_elbo(ELBO_hist, elbo_iter, elbo_warmup_iter, t, dt_flow, batch_size, batch_size, num_layers, elbo_lr, elbo_lr_decay_step_size, elbo_warmup_lr, prior_scale_factor, plots_folder, now_string, xmin = elbo_warmup_iter + int(elbo_iter / 2))
