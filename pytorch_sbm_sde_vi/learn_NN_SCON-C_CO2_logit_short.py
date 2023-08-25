18225001b080ee5615849e8da2e0398d90ab1495
Wally Xie <x@wallyxie.com>
2023-08-25 13:37:11 -0700

    Update learn_NN_SCON-C_CO2_logit_short.py

--- pytorch_sbm_sde_vi/learn_NN_SCON-C_CO2_logit_short.py
+++ pytorch_sbm_sde_vi/learn_NN_SCON-C_CO2_logit_short.py
@@ -49,9 +49,9 @@ temp_ref = 283
 temp_rise = 5 #High estimate of 5 celsius temperature rise by 2100.
 
 #Training parameters
-elbo_iter = 150000
-elbo_lr = 5e-4
-elbo_lr_decay = 0.8
+elbo_iter = 140000
+elbo_lr = 3e-4
+elbo_lr_decay = 0.85
 elbo_lr_decay_step_size = 10000
 elbo_warmup_iter = 10000
 elbo_warmup_lr = 1e-6
