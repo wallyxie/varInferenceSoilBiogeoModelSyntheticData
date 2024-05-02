fb063cfbcdcff56f51fa0869f1d57b51616909bc
Hua Xie <x@wallyxie.com>
2024-05-02 14:15:43 -0700

    Plot more of ELBO trajectory in non-max log p decomp experiments to see more global convergence

--- pytorch_sbm_sde_vi/learn_theta_SCON-SS_CO2_logit_short_log_p.py
+++ pytorch_sbm_sde_vi/learn_theta_SCON-SS_CO2_logit_short_log_p.py
@@ -197,7 +197,7 @@ print('Output files saving finished. Moving to plotting.')
 with torch.no_grad():
     x, _ = net(eval_batch_size)
 plots_folder = 'training_plots/'
-plot_elbo(ELBO_hist, elbo_iter, elbo_warmup_iter, t, dt_flow, batch_size, eval_batch_size, num_layers, elbo_lr, elbo_lr_decay_step_size, elbo_warmup_lr, prior_scale_factor, plots_folder, now_string, xmin = elbo_warmup_iter + int(elbo_iter / 2))
+plot_elbo(ELBO_hist, elbo_iter, elbo_warmup_iter, t, dt_flow, batch_size, eval_batch_size, num_layers, elbo_lr, elbo_lr_decay_step_size, elbo_warmup_lr, prior_scale_factor, plots_folder, now_string, xmin = elbo_warmup_iter + int(elbo_iter / 4))
 print('ELBO plotting finished.')
 plot_states_post(x, q_theta, obs_model, SBM_SDE_instance, elbo_iter, elbo_warmup_iter, t, dt_flow, batch_size, eval_batch_size, num_layers, elbo_lr, elbo_lr_decay_step_size, elbo_warmup_lr, prior_scale_factor, plots_folder, now_string, fix_theta_dict, learn_CO2, ymin_list = [0, 0, 0, 0], ymax_list = [70., 5., 8., 0.03])
 print('States fit plotting finished.')
