66ccfca12cf6d0da33504e0fe42ba92685bd9088
Hua Xie <x@wallyxie.com>
2024-05-03 00:30:01 -0700

    Changed training hyperparams of learn NN only experiments because of gradient nan

--- pytorch_sbm_sde_vi/learn_NN_SCON-C_CO2_logit_short_log_p.py
+++ pytorch_sbm_sde_vi/learn_NN_SCON-C_CO2_logit_short_log_p.py
@@ -50,8 +50,8 @@ temp_ref = 283
 temp_rise = 5 #High estimate of 5 celsius temperature rise by 2100.
 
 #Training parameters
-elbo_iter = 230000
-elbo_lr = 4e-4
+elbo_iter = 240000
+elbo_lr = 3.8e-4
 elbo_lr_decay = 0.7
 elbo_lr_decay_step_size = 10000
 elbo_warmup_iter = 20000
