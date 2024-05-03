8c2d82136b8b794bff410da678251f1e9d8d48b5
Hua Xie <x@wallyxie.com>
2024-05-03 08:19:31 -0700

    Updated training hyperparams for learn NN only experiment

--- pytorch_sbm_sde_vi/learn_NN_SCON-C_CO2_logit_short.py
+++ pytorch_sbm_sde_vi/learn_NN_SCON-C_CO2_logit_short.py
@@ -51,7 +51,7 @@ temp_rise = 5 #High estimate of 5 celsius temperature rise by 2100.
 
 #Training parameters
 elbo_iter = 230000
-elbo_lr = 4e-4
+elbo_lr = 3.8e-4
 elbo_lr_decay = 0.7
 elbo_lr_decay_step_size = 10000
 elbo_warmup_iter = 20000
